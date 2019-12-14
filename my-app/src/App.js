8cf526bd32b5f34fe53eed4b9b57e11859316252
cqxg <cqxg@yandex.ru>
2019-12-14 16:48:31 +0300

    feat: add simple component structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 
 import './App.css';
@@ -7,46 +10,9 @@ import './App.css';
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img src=''></img>
-            </header>
-            <nav className='nav'>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className='content'>
-                <img src='https://a.d-cd.net/870b45as-960.jpg'></img>
-            </div>
-            <div>
-                ava+desc
-            </div>
-            <div>
-                My Posts
-            </div>
-            <div>
-                new post
-            </div>
-            <div>
-                <div>
-                    Post1
-                </div>
-                <div>
-                    Poast2
-                </div>
-            </div>
+            <Header />
+            <Navbar />
+            <Profile />
         </div>
     );
 }
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src=''></img>
+        </header>
+    )
+}
+
+export default Header;
