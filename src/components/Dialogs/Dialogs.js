098d5ca7b0700168ebeee810257cc6bb5353da4c
cqxg <cqxg@yandex.ru>
2020-01-02 16:53:06 +0300

    feat: add message page workpiece

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,10 +1,35 @@
 import React from 'react';
+
 import style from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog + ' ' + style.active}>
+                    105
+                </div>
+                <div className={style.dialog}>
+                    FrinteZz
+                </div>
+                <div className={style.dialog}>
+                    Gaming
+                </div>
+                <div className={style.dialog}>
+                    Pominki
+                </div>
+                <div className={style.dialog}>
+                    Side
+                </div>
+                <div className={style.dialog}>
+                    EFsquad
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>qq all</div>
+                <div className={style.message}>How are you?</div>
+                <div className={style.message}>gl gg wp</div>
+            </div>
         </div>
     )
 }
