fc0013111b494809d823a6f1b028f21aa49a1622
cqxg <cqxg@yandex.ru>
2020-01-04 17:08:24 +0300

    feat: add start color scheme change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 
-
 import './App.css';
 
 const App = () => (
