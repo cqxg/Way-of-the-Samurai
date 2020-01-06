4e4b66811ccde7688c0bae0531c9a8a6cafeb9a0
cqxg <cqxg@yandex.ru>
2020-01-06 17:37:10 +0300

    feat: add input cleaning

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -5,30 +5,31 @@ import Post from './Post/Post';
 import style from './MyPosts.module.css';
 
 const MyPosts = (props) => {
-  const postsElements = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount} />);
-  const newPostElement = React.createRef();
+    const postsElements = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount} />);
+    const newPostElement = React.createRef();
 
-  const addPost = () => {
-    const text = newPostElement.current.value;
-    props.addPost(text);
-  };
+    const addPost = () => {
+        const text = newPostElement.current.value;
+        props.addPost(text);
+        newPostElement.current.value = '';
+    };
 
-  return (
-    <div className={style.postsBlock}>
-      <h3>My Posts</h3>
-      <div>
-        <div>
-          <textarea ref={newPostElement} />
+    return (
+        <div className={style.postsBlock}>
+            <h3>My Posts</h3>
+            <div>
+                <div>
+                    <textarea ref={newPostElement} />
+                </div>
+                <div>
+                    <button onClick={addPost}>Add Post</button>
+                </div>
+            </div>
+            <div className={style.posts}>
+                {postsElements}
+            </div>
         </div>
-        <div>
-          <button onClick={addPost}>Add Post</button>
-        </div>
-      </div>
-      <div className={style.posts}>
-        {postsElements}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default MyPosts;
