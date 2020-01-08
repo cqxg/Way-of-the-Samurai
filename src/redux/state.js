02466b5f21c4dbb983bb38fde9c84ee3a71c72f9
cqxg <cqxg@yandex.ru>
2020-01-08 13:25:57 +0300

    feat: add store

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,88 +1,91 @@
-const state = {
-  profilePage: {
-    posts: [
-      {
-        id: '1',
-        message: 'Hi, how are you',
-        likesCount: '12',
-      },
-      {
-        id: '2',
-        message: 'It`s my first posts',
-        likesCount: '7',
-      },
-    ],
-    newPostText: '',
-  },
-  dialogsPage: {
-    dialogs: [
-      {
-        id: '1',
-        name: '105',
-      },
-      {
-        id: '2',
-        name: 'FrinteZz',
-      },
-      {
-        id: '3',
-        name: 'Gaming',
-      },
-      {
-        id: '4',
-        name: 'Pominki',
-      },
-      {
-        id: '5',
-        name: 'Side',
-      },
-      {
-        id: '6',
-        name: 'EFsquad',
-      },
-    ],
+const store = {
+    _state = {
+        profilePage: {
+            posts: [
+                {
+                    id: '1',
+                    message: 'Hi, how are you',
+                    likesCount: '12',
+                },
+                {
+                    id: '2',
+                    message: 'It`s my first posts',
+                    likesCount: '7',
+                },
+            ],
 
-    messages: [
-      {
-        id: '1',
-        message: 'qq all',
-      },
-      {
-        id: '2',
-        message: 'hello world',
-      },
-      {
-        id: '3',
-        message: 'priveti4 ot Marmeladi4a',
-      },
-    ],
-  },
+            newPostText: '',
+        },
+
+        dialogsPage: {
+            dialogs: [
+                {
+                    id: '1',
+                    name: '105',
+                },
+                {
+                    id: '2',
+                    name: 'FrinteZz',
+                },
+                {
+                    id: '3',
+                    name: 'Gaming',
+                },
+                {
+                    id: '4',
+                    name: 'Pominki',
+                },
+                {
+                    id: '5',
+                    name: 'Side',
+                },
+                {
+                    id: '6',
+                    name: 'EFsquad',
+                },
+            ],
+
+            messages: [
+                {
+                    id: '1',
+                    message: 'qq all',
+                },
+                {
+                    id: '2',
+                    message: 'hello world',
+                },
+                {
+                    id: '3',
+                    message: 'priveti4 ot Marmeladi4a',
+                },
+            ],
+        },
+    },
+
+    rerenderEntireTree() {
+        console.log('changed');
+    },
+
+    addPost() {
+        const newPost = {
+            id: 7,
+            message: state.profilePage.newPostText,
+            likesCount: 0,
+        };
+
+        state.profilePage.posts.push(newPost);
+        state.profilePage.newPostText = '';
+        rerenderEntireTree(state);
+    },
+
+    updateNewPostText(newText) {
+        state.profilePage.newPostText = newText;
+        rerenderEntireTree(state);
+    },
+
+    subscribe(observer) {
+        rerenderEntireTree = observer;
+    }
 };
 
-let rerenderEntireTree = () => {
-  console.log('changed');
-};
-
-export const addPost = () => {
-  const newPost = {
-    id: 7,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-
-export default state;
+export default store;
