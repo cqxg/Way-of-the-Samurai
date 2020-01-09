b848cb80a5a4101c35670ad8eed8e25e08242d87
cqxg <cqxg@yandex.ru>
2020-01-10 02:16:43 +0300

    feat: add action type emulation

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -7,34 +7,34 @@ import { updateNewPostTextActionCreator, addPostActionCreator } from '../../../r
 import style from './MyPosts.module.css';
 
 const MyPosts = (props) => {
-    const postsElements = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount} />);
-    const newPostElement = React.createRef();
+  const postsElements = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount} />);
+  const newPostElement = React.createRef();
 
-    const addPost = () => {
-        props.dispatch(addPostActionCreator());
-    };
+  const addPost = () => {
+    props.dispatch(addPostActionCreator());
+  };
 
-    const onPostChange = () => {
-        const text = newPostElement.current.value;
-        props.dispatch(updateNewPostTextActionCreator(text));
-    };
+  const onPostChange = () => {
+    const text = newPostElement.current.value;
+    props.dispatch(updateNewPostTextActionCreator(text));
+  };
 
-    return (
-        <div className={style.postsBlock}>
-            <h3>My Posts</h3>
-            <div>
-                <div>
-                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
-                </div>
-                <div>
-                    <button onClick={addPost}>Add Post</button>
-                </div>
-            </div>
-            <div className={style.posts}>
-                {postsElements}
-            </div>
+  return (
+    <div className={style.postsBlock}>
+      <h3>My Posts</h3>
+      <div>
+        <div>
+          <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
         </div>
-    );
+        <div>
+          <button onClick={addPost}>Add Post</button>
+        </div>
+      </div>
+      <div className={style.posts}>
+        {postsElements}
+      </div>
+    </div>
+  );
 };
 
 export default MyPosts;
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,109 +1,106 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 const store = {
 
-    _state: {
-        profilePage: {
-            posts: [
-                {
-                    id: '1',
-                    message: 'Hi, how are you',
-                    likesCount: '12',
-                },
-                {
-                    id: '2',
-                    message: 'It`s my first posts',
-                    likesCount: '7',
-                },
-            ],
-
-            newPostText: '',
+  _state: {
+    profilePage: {
+      posts: [
+        {
+          id: '1',
+          message: 'Hi, how are you',
+          likesCount: '12',
         },
-
-        dialogsPage: {
-            dialogs: [
-                {
-                    id: '1',
-                    name: '105',
-                },
-                {
-                    id: '2',
-                    name: 'FrinteZz',
-                },
-                {
-                    id: '3',
-                    name: 'Gaming',
-                },
-                {
-                    id: '4',
-                    name: 'Pominki',
-                },
-                {
-                    id: '5',
-                    name: 'Side',
-                },
-                {
-                    id: '6',
-                    name: 'EFsquad',
-                },
-            ],
-
-            messages: [
-                {
-                    id: '1',
-                    message: 'qq all',
-                },
-                {
-                    id: '2',
-                    message: 'hello world',
-                },
-                {
-                    id: '3',
-                    message: 'priveti4 ot Marmeladi4a',
-                },
-            ],
+        {
+          id: '2',
+          message: 'It`s my first posts',
+          likesCount: '7',
         },
+      ],
+
+      newPostText: '',
     },
 
-    getState() {
-        return this._state;
+    dialogsPage: {
+      dialogs: [
+        {
+          id: '1',
+          name: '105',
+        },
+        {
+          id: '2',
+          name: 'FrinteZz',
+        },
+        {
+          id: '3',
+          name: 'Gaming',
+        },
+        {
+          id: '4',
+          name: 'Pominki',
+        },
+        {
+          id: '5',
+          name: 'Side',
+        },
+        {
+          id: '6',
+          name: 'EFsquad',
+        },
+      ],
+
+      messages: [
+        {
+          id: '1',
+          message: 'qq all',
+        },
+        {
+          id: '2',
+          message: 'hello world',
+        },
+        {
+          id: '3',
+          message: 'priveti4 ot Marmeladi4a',
+        },
+      ],
     },
+  },
 
-    _callSubscriber() {
-        console.log('changed');
-    },
+  getState() {
+    return this._state;
+  },
 
-    subscribe(observer) {
-        this._callSubscriber = observer;
-    },
+  _callSubscriber() {
+    console.log('changed');
+  },
 
-    dispatch(action) {
-        if (action.type === 'ADD-POST') {
-            const newPost = {
-                id: 7,
-                message: this._state.profilePage.newPostText,
-                likesCount: 0,
-            };
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
 
-            this._state.profilePage.posts.push(newPost);
-            this._state.profilePage.newPostText = '';
-            this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
-            this._state.profilePage.newPostText = action.newText;
-            this._callSubscriber(this._state);
-        }
-    },
-};
+  dispatch(action) {
+    if (action.type === ADD_POST) {
+      const newPost = {
+        id: 7,
+        message: this._state.profilePage.newPostText,
+        likesCount: 0,
+      };
 
-export const addPostActionCreator = () => {
-    return {
-        type: 'ADD-POST'
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = '';
+      this._callSubscriber(this._state);
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);
     }
+  },
 };
 
-export const updateNewPostTextActionCreator = (text) => {
-    return {
-        type: 'UPDATE-NEW-POST-TEXT',
-        newText: text
-    }
-};
+export const addPostActionCreator = () => ({ type: ADD_POST });
+
+export const updateNewPostTextActionCreator = (text) => ({
+  type: UPDATE_NEW_POST_TEXT,
+  newText: text,
+});
 
 export default store;
