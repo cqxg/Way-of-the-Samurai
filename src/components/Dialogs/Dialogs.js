9fdf17dc44b109b82c6ff0fea683d9f4208e4a23
cqxg <cqxg@yandex.ru>
2020-01-11 11:46:30 +0300

    feat: add message page preparation

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -2,36 +2,48 @@ import React from 'react';
 
 import DialogItem from './DialogsItem/DialogsItem';
 import Message from './Message/Message';
+import { updateNewMessageBodyCreator, sendMessageCreator } from '../../redux/state';
 
 import style from './Dialogs.module.css';
 
 const Dialogs = (props) => {
-  const { dialogs } = props.state;
-  const { messages } = props.state;
+    const { dialogs } = props.state;
+    const { messages } = props.state;
 
-  const dialogsElements = dialogs.map((dialog) => <DialogItem name={dialog.name} id={dialog.id} />);
-  const messagesElements = messages.map((message) => <Message message={message.message} />);
-  const newDialogText = React.createRef();
+    const dialogsElements = dialogs.map((dialog) => <DialogItem name={dialog.name} id={dialog.id} />);
+    const messagesElements = messages.map((message) => <Message message={message.message} />);
+    const newMessageBody = props.state.newMessageBody;
+    // const newDialogText = React.createRef();
 
-  const showDialogText = () => {
-    const text = newDialogText.current.value;
-    alert(text);
-  };
+    const onSendMessageClick = () => {
+        props.store.dispatch(sendMessageCreator());
+    };
 
-  return (
-    <div className={style.dialogs}>
-      <div className={style.dialogsItems}>
-        {dialogsElements}
-      </div>
-      <div className={style.messages}>
-        {messagesElements}
-      </div>
-      <div>
-        <textarea ref={newDialogText} />
-        <button onClick={showDialogText} />
-      </div>
-    </div>
-  );
+    const onNewMessageChange = (e) => {
+        const body = e.target.value;
+        props.store.dispatch(updateNewMessageBodyCreator(body));
+    };
+
+    return (
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                {dialogsElements}
+            </div>
+            <div className={style.messages}>
+                {messagesElements}
+                <div>
+                    <div>
+                        <textarea
+                            value={newMessageBody}
+                            onChange={onNewMessageChange}
+                            placeholder='Add u message'>
+                        </textarea>
+                    </div>
+                    <div><button onClick={onSendMessageClick}>Send</button></div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Dialogs;
