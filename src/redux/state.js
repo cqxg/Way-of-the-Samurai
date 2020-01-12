54a7ddef1114f209de38f90d184a126eed21a025
cqxg <cqxg@yandex.ru>
2020-01-12 15:21:45 +0300

    fix: removed '_' from variable names

--- src/redux/state.js
+++ src/redux/state.js
@@ -5,122 +5,122 @@ const SEND_MESSAGE = 'SEND-MESSAGE';
 
 const store = {
 
-  _state: {
-    profilePage: {
-      posts: [
-        {
-          id: '1',
-          message: 'Hi, how are you',
-          likesCount: '12',
-        },
-        {
-          id: '2',
-          message: 'It`s my first posts',
-          likesCount: '7',
-        },
-      ],
+    state: {
+        profilePage: {
+            posts: [
+                {
+                    id: '1',
+                    message: 'Hi, how are you',
+                    likesCount: '12',
+                },
+                {
+                    id: '2',
+                    message: 'It`s my first posts',
+                    likesCount: '7',
+                },
+            ],
 
-      newPostText: '',
+            newPostText: '',
+        },
+
+        dialogsPage: {
+            dialogs: [
+                {
+                    id: '1',
+                    name: '105',
+                },
+                {
+                    id: '2',
+                    name: 'FrinteZz',
+                },
+                {
+                    id: '3',
+                    name: 'Gaming',
+                },
+                {
+                    id: '4',
+                    name: 'Pominki',
+                },
+                {
+                    id: '5',
+                    name: 'Side',
+                },
+                {
+                    id: '6',
+                    name: 'EFsquad',
+                },
+            ],
+
+            messages: [
+                {
+                    id: '1',
+                    message: 'qq all',
+                },
+                {
+                    id: '2',
+                    message: 'hello world',
+                },
+                {
+                    id: '3',
+                    message: 'priveti4 ot Marmeladi4a',
+                },
+            ],
+
+            newMessageBody: '',
+
+        },
     },
 
-    dialogsPage: {
-      dialogs: [
-        {
-          id: '1',
-          name: '105',
-        },
-        {
-          id: '2',
-          name: 'FrinteZz',
-        },
-        {
-          id: '3',
-          name: 'Gaming',
-        },
-        {
-          id: '4',
-          name: 'Pominki',
-        },
-        {
-          id: '5',
-          name: 'Side',
-        },
-        {
-          id: '6',
-          name: 'EFsquad',
-        },
-      ],
-
-      messages: [
-        {
-          id: '1',
-          message: 'qq all',
-        },
-        {
-          id: '2',
-          message: 'hello world',
-        },
-        {
-          id: '3',
-          message: 'priveti4 ot Marmeladi4a',
-        },
-      ],
-
-      newMessageBody: '',
-
+    getState() {
+        return this.state;
     },
-  },
 
-  getState() {
-    return this._state;
-  },
+    callSubscriber() {
+        console.log('changed');
+    },
 
-  _callSubscriber() {
-    console.log('changed');
-  },
+    subscribe(observer) {
+        this.callSubscriber = observer;
+    },
 
-  subscribe(observer) {
-    this._callSubscriber = observer;
-  },
+    dispatch(action) {
+        if (action.type === ADD_POST) {
+            const newPost = {
+                id: 7,
+                message: this.state.profilePage.newPostText,
+                likesCount: 0,
+            };
 
-  dispatch(action) {
-    if (action.type === ADD_POST) {
-      const newPost = {
-        id: 7,
-        message: this._state.profilePage.newPostText,
-        likesCount: 0,
-      };
-
-      this._state.profilePage.posts.push(newPost);
-      this._state.profilePage.newPostText = '';
-      this._callSubscriber(this._state);
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-      this._state.profilePage.newPostText = action.newText;
-      this._callSubscriber(this._state);
-    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-      this._state.dialogsPage.newMessageBody = action.body;
-      this._callSubscriber(this._state);
-    } else if (action.type === SEND_MESSAGE) {
-      const body = this._state.dialogsPage.newMessageBody;
-      this._state.dialogsPage.newMessageBody = '';
-      this._state.dialogsPage.messages.push({ id: 8, message: body });
-      this._callSubscriber(this._state);
-    }
-  },
+            this.state.profilePage.posts.push(newPost);
+            this.state.profilePage.newPostText = '';
+            this.callSubscriber(this.state);
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
+            this.state.profilePage.newPostText = action.newText;
+            this.callSubscriber(this.state);
+        } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+            this.state.dialogsPage.newMessageBody = action.body;
+            this.callSubscriber(this.state);
+        } else if (action.type === SEND_MESSAGE) {
+            const body = this.state.dialogsPage.newMessageBody;
+            this.state.dialogsPage.newMessageBody = '';
+            this.state.dialogsPage.messages.push({ id: 8, message: body });
+            this.callSubscriber(this.state);
+        }
+    },
 };
 
 export const addPostActionCreator = () => ({ type: ADD_POST });
 
 export const updateNewPostTextActionCreator = (text) => ({
-  type: UPDATE_NEW_POST_TEXT,
-  newText: text,
+    type: UPDATE_NEW_POST_TEXT,
+    newText: text,
 });
 
 export const sendMessageCreator = () => ({ type: SEND_MESSAGE });
 
 export const updateNewMessageBodyCreator = (body) => ({
-  type: UPDATE_NEW_MESSAGE_BODY,
-  body,
+    type: UPDATE_NEW_MESSAGE_BODY,
+    body,
 });
 
 export default store;
