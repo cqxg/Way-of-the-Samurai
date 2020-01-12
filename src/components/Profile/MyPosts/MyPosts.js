69d1188946775369da59bb3617d2e8c209ce1e06
cqxg <cqxg@yandex.ru>
2020-01-12 15:48:30 +0300

    fix: breaking this return JSX into MyPosts

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -7,34 +7,40 @@ import Post from './Post/Post';
 import style from './MyPosts.module.css';
 
 const MyPosts = (props) => {
-  const postsElements = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount} />);
-  const newPostElement = React.createRef();
+    const postsElements = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount} />);
+    const newPostElement = React.createRef();
 
-  const addPost = () => {
-    props.dispatch(addPostActionCreator());
-  };
+    const textRender = () => {
+        return (
+            <div>
+                <div>
+                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
+                </div>
+                <div>
+                    <button onClick={addPost}>Add Post</button>
+                </div>
+            </div>
+        );
+    };
 
-  const onPostChange = () => {
-    const text = newPostElement.current.value;
-    props.dispatch(updateNewPostTextActionCreator(text));
-  };
+    const addPost = () => {
+        props.dispatch(addPostActionCreator());
+    };
 
-  return (
-    <div className={style.postsBlock}>
-      <h3>My Posts</h3>
-      <div>
-        <div>
-          <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
+    const onPostChange = () => {
+        const text = newPostElement.current.value;
+        props.dispatch(updateNewPostTextActionCreator(text));
+    };
+
+    return (
+        <div className={style.postsBlock}>
+            <h3>My Posts</h3>
+            {textRender()}
+            <div className={style.posts}>
+                {postsElements}
+            </div>
         </div>
-        <div>
-          <button onClick={addPost}>Add Post</button>
-        </div>
-      </div>
-      <div className={style.posts}>
-        {postsElements}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default MyPosts;
