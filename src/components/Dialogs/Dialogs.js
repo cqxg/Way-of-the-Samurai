61793b17f5a77b691a2210f5e8087159593afb61
cqxg <cqxg@yandex.ru>
2020-01-12 14:45:58 +0300

    fix: distructuring statements at the beginning of the function

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -8,40 +8,40 @@ import Message from './Message/Message';
 import style from './Dialogs.module.css';
 
 const Dialogs = (props) => {
-  const state = props.store.getState().dialogsPage;
-  const dialogsElements = state.dialogs.map((dialog) => <DialogItem name={dialog.name} id={dialog.id} />);
-  const messagesElements = state.messages.map((message) => <Message message={message.message} />);
-  const { newMessageBody } = state;
+    const { newMessageBody } = state;
+    const state = props.store.getState().dialogsPage;
+    const dialogsElements = state.dialogs.map((dialog) => <DialogItem name={dialog.name} id={dialog.id} />);
+    const messagesElements = state.messages.map((message) => <Message message={message.message} />);
 
-  const onSendMessageClick = () => {
-    props.store.dispatch(sendMessageCreator());
-  };
+    const onSendMessageClick = () => {
+        props.store.dispatch(sendMessageCreator());
+    };
 
-  const onNewMessageChange = (e) => {
-    const body = e.target.value;
-    props.store.dispatch(updateNewMessageBodyCreator(body));
-  };
+    const onNewMessageChange = (e) => {
+        const body = e.target.value;
+        props.store.dispatch(updateNewMessageBodyCreator(body));
+    };
 
-  return (
-    <div className={style.dialogs}>
-      <div className={style.dialogsItems}>
-        {dialogsElements}
-      </div>
-      <div className={style.messages}>
-        {messagesElements}
-        <div>
-          <div>
-            <textarea
-              value={newMessageBody}
-              onChange={onNewMessageChange}
-              placeholder="Add u message"
-            />
-          </div>
-          <div><button onClick={onSendMessageClick}>Send</button></div>
+    return (
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                {dialogsElements}
+            </div>
+            <div className={style.messages}>
+                {messagesElements}
+                <div>
+                    <div>
+                        <textarea
+                            value={newMessageBody}
+                            onChange={onNewMessageChange}
+                            placeholder="Add u message"
+                        />
+                    </div>
+                    <div><button onClick={onSendMessageClick}>Send</button></div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Dialogs;
