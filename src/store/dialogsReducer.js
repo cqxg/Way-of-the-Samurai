c39cd94c0ebfb36c18bc559f73c499cfec8ca8fd
cqxg <cqxg@yandex.ru>
2020-01-13 13:19:19 +0300

    fix: reducers refactoring

--- src/store/dialogsReducer.js
+++ src/store/dialogsReducer.js
@@ -2,14 +2,19 @@ import { UPDATE_NEW_MESSAGE_BODY, SEND_MESSAGE } from './actionTypes';
 
 export const dialogsReducer = (state, action) => {
 
-    if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-        state.newMessageBody = action.body;
+    switch (action.type) {
+        case UPDATE_NEW_MESSAGE_BODY:
+            state.newMessageBody = action.body;
+            return state;
+            
+        case SEND_MESSAGE:
+            const body = state.newMessageBody;
 
-    } else if (action.type === SEND_MESSAGE) {
-        const body = state.newMessageBody;
+            state.newMessageBody = '';
+            state.messages.push({ id: 8, message: body });
+            return state;
 
-        state.newMessageBody = '';
-        state.messages.push({ id: 8, message: body });
-    }
-    return state
+        default:
+            return state;
+    };
 };
--- src/store/profileReducer.js
+++ src/store/profileReducer.js
@@ -2,18 +2,24 @@ import { ADD_POST, UPDATE_NEW_POST_TEXT } from './actionTypes';
 
 export const profileReducer = (state, action) => {
 
-    if (action.type === ADD_POST) {
-        const newPost = {
-            id: 7,
-            message: state.newPostText,
-            likesCount: 0,
-        };
-        state.posts.push(newPost);
-        state.newPostText = '';
+    switch (action.type) {
+        case ADD_POST:
+            const newPost = {
+                id: 7,
+                message: state.newPostText,
+                likesCount: 0,
+            };
 
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-        state.newPostText = action.newText;
+            state.posts.push(newPost);
+            state.newPostText = '';
+            return state;
+
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newText;
+            return state;
+
+        default:
+            return state;
+
     };
-
-    return state
 };
