13f8129928e0b76e581ffdcca318efbf40cec180
cqxg <cqxg@yandex.ru>
2020-01-15 23:14:00 +0300

    fix: renaming reduxStore to store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import store from './redux/reduxStore';
+import store from './redux/store';
 import App from './App';
 
 import './index.css';
