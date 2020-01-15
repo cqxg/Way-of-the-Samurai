e97a823a3a00323dd7c4046082bdce45ccf36488
cqxg <cqxg@yandex.ru>
2020-01-15 23:49:39 +0300

    fix: profileReducer

--- src/store/profileReducer.js
+++ src/store/profileReducer.js
@@ -1,51 +1,50 @@
 import { ADD_POST, UPDATE_NEW_POST_TEXT } from './actions/actionTypes';
 
 const initialState = {
-  posts: [
-    {
-      id: '1',
-      message: 'Hi, how are you',
-      likesCount: '12',
-    },
-    {
-      id: '2',
-      message: 'It`s my first posts',
-      likesCount: '7',
-    },
-  ],
+    posts: [
+        {
+            id: '1',
+            message: 'Hi, how are you',
+            likesCount: '12',
+        },
+        {
+            id: '2',
+            message: 'It`s my first posts',
+            likesCount: '7',
+        },
+    ],
 
-  newPostText: '',
+    newPostText: '',
 };
 
 const defaultLikesValue = 0;
 
 const profileReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_POST: {
-      const newPost = {
-        id: 7,
-        message: state.newPostText,
-        likesCount: defaultLikesValue,
-      };
+    switch (action.type) {
+        case ADD_POST: {
+            const newPost = {
+                id: 7,
+                message: state.newPostText,
+                likesCount: defaultLikesValue,
+            };
 
-      const stateCopy = { ...state };
+            return {
+                ...state,
+                newPostText: '',
+                posts: [...state.posts, newPost],
+            };
+        };
 
-      stateCopy.posts = [...state.posts];
-      stateCopy.posts.push(newPost);
-      stateCopy.newPostText = '';
-      return stateCopy;
-    }
+        case UPDATE_NEW_POST_TEXT: {
+            return {
+                ...state,
+                newPostText: action.newText
+            };
+        };
 
-    case UPDATE_NEW_POST_TEXT: {
-      const stateCopy = { ...state };
-
-      stateCopy.newPostText = action.newText;
-      return stateCopy;
-    }
-
-    default:
-      return state;
-  }
+        default:
+            return state;
+    };
 };
 
 export { profileReducer };
