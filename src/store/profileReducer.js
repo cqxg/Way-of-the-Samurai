8eb39e28bb623f5d9cf77cd8bbd96631f8df9ea9
cqxg <cqxg@yandex.ru>
2020-01-15 23:54:08 +0300

    fix: lint:fix

--- src/store/profileReducer.js
+++ src/store/profileReducer.js
@@ -1,51 +1,51 @@
 import { ADD_POST, UPDATE_NEW_POST_TEXT } from './actions/actionTypes';
 
 const initialState = {
-    posts: [
-        {
-            id: '1',
-            message: 'Hi, how are you',
-            likesCount: '12',
-        },
-        {
-            id: '2',
-            message: 'It`s my first posts',
-            likesCount: '7',
-        },
-    ],
+  posts: [
+    {
+      id: '1',
+      message: 'Hi, how are you',
+      likesCount: '12',
+    },
+    {
+      id: '2',
+      message: 'It`s my first posts',
+      likesCount: '7',
+    },
+  ],
 
-    newPostText: '',
+  newPostText: '',
 };
 
 const defaultLikesValue = 0;
 const nextId = 8;
 
 const profileReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_POST: {
-            const newPost = {
-                id: nextId,
-                message: state.newPostText,
-                likesCount: defaultLikesValue,
-            };
+  switch (action.type) {
+    case ADD_POST: {
+      const newPost = {
+        id: nextId,
+        message: state.newPostText,
+        likesCount: defaultLikesValue,
+      };
 
-            return {
-                ...state,
-                newPostText: '',
-                posts: [...state.posts, newPost],
-            };
-        };
+      return {
+        ...state,
+        newPostText: '',
+        posts: [...state.posts, newPost],
+      };
+    }
 
-        case UPDATE_NEW_POST_TEXT: {
-            return {
-                ...state,
-                newPostText: action.newText
-            };
-        };
+    case UPDATE_NEW_POST_TEXT: {
+      return {
+        ...state,
+        newPostText: action.newText,
+      };
+    }
 
-        default:
-            return state;
-    };
+    default:
+      return state;
+  }
 };
 
 export { profileReducer };
