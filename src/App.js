477200b3c0a16101c2ca3e8af3df91a36429db2c
cqxg <cqxg@yandex.ru>
2020-01-16 11:55:25 +0300

    feat: add users component

--- src/App.js
+++ src/App.js
@@ -7,24 +7,26 @@ import Profile from './components/Profile/Profile';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import News from './components/News/News';
 import Music from './components/Music/Music';
+import Users from './components/Users/Users'
 import Settings from './components/Settings/Settings';
 
 import './App.css';
 
 const App = () => (
-  <BrowserRouter>
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Route path="/profile" render={() => <Profile />} />
-        <Route path="/dialogs" render={() => <DialogsContainer />} />
-        <Route path="/news" render={() => <News />} />
-        <Route path="/music" render={() => <Music />} />
-        <Route path="/settings" render={() => <Settings />} />
-      </div>
-    </div>
-  </BrowserRouter>
+    <BrowserRouter>
+        <div className="app-wrapper">
+            <Header />
+            <Navbar />
+            <div className="app-wrapper-content">
+                <Route path="/profile" render={() => <Profile />} />
+                <Route path="/dialogs" render={() => <DialogsContainer />} />
+                <Route path="/news" render={() => <News />} />
+                <Route path="/music" render={() => <Music />} />
+                <Route path="/users" render={() => <Users />} />
+                <Route path="/settings" render={() => <Settings />} />
+            </div>
+        </div>
+    </BrowserRouter>
 );
 
 export default App;
---
+++ src/components/Users/Users.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <div>
+            USERS WILL BE HERE
+        </div>
+    );
+};
+
+export default Users;
