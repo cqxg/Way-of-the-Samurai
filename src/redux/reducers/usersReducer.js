381692867bcd05eb3f74cd8029df6148880fcf34
cqxg <cqxg@yandex.ru>
2020-01-16 21:29:20 +0300

    add usersReducer worpiece

--- src/redux/reducers/usersReducer.js
+++ src/redux/reducers/usersReducer.js
@@ -1,51 +1,49 @@
-import { ADD_POST, UPDATE_NEW_POST_TEXT } from '../actions/actionTypes';
+import { FOLLOW, UNFOLLOW } from '../actions/actionTypes';
 
 const initialState = {
-  posts: [
-    {
-      id: '1',
-      message: 'Hi, how are you',
-      likesCount: '12',
-    },
-    {
-      id: '2',
-      message: 'It`s my first posts',
-      likesCount: '7',
-    },
-  ],
-
-  newPostText: '',
+    users: [
+        {
+            id: '1',
+            followed: true,
+            fullName: 'Vasiliy',
+            status: 'vasiliy propionate',
+            location: {
+                city: 'Giran Harbor',
+                country: 'Elmoraden'
+            },
+        },
+        {
+            id: '2',
+            followed: true,
+            fullName: 'Gennadiy',
+            status: 'vse dl9 pacanov',
+            location: {
+                city: 'Vorota Diona',
+                country: 'Dion'
+            },
+        },
+        {
+            id: '3',
+            followed: true,
+            fullName: 'Tatiyana',
+            status: 'Tanysha podai grabli!!!1',
+            location: {
+                city: 'Minsk',
+                country: 'Belarus'
+            },
+        },
+    ],
 };
-
-const defaultLikesValue = 0;
-const nextId = 8;
 
 const usersReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_POST: {
-      const newPost = {
-        id: nextId,
-        message: state.newPostText,
-        likesCount: defaultLikesValue,
-      };
+    switch (action.type) {
 
-      return {
-        ...state,
-        newPostText: '',
-        posts: [...state.posts, newPost],
-      };
+        default:
+            return state;
     };
-
-    case UPDATE_NEW_POST_TEXT: {
-      return {
-        ...state,
-        newPostText: action.newText,
-      };
-    };
-
-    default:
-      return state;
-  };
 };
+
+export const followActionCreator = () => ({ type: FOLLOW });
+export const unfollowActionCreator = () => ({ type: UNFOLLOW });
 
 export { usersReducer };
