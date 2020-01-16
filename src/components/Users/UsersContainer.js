f7c45d17aa73f24c987eb4217ecb4fb2aa0b70f5
cqxg <cqxg@yandex.ru>
2020-01-16 22:06:35 +0300

    feat: add UsersContainer workpiece

---
+++ src/components/Users/UsersContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import Users from './Users';
+
+const mapStateToProps = (state) => {
+    return {
+
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
