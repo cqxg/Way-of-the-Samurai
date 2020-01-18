b3e9eb9dc8dc7fed0d91354bbb86b8cad7900b53
cqxg <cqxg@yandex.ru>
2020-01-18 15:19:01 +0300

    fix: users page

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,51 +1,73 @@
 import React from 'react';
 
-// import Axios from 'axios';
+// import * as axios from 'axios';
+
+// axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+//     props.setUsers()
+// });
 
 import styles from './Users.module.css';
 
 const Users = (props) => {
 
-    // axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-    //     props.setUsers()
-    // });
+    const span1 = (user) => {
+        const { photoUrl, id, followed } = user;
 
-    <div>
-        {
-            props.users.map((user) => (
-                <div key={user.id}>
-                    <span>
-                        <div>
-                            <img src={user.photoUrl} className={styles.userPhoto} />
-                        </div>
-                        <div>
-                            {user.followed
-                                ? <button onClick={() => { props.unfollow(user.id); }}>Unfollow</button>
-                                : <button onClick={() => { props.follow(user.id); }}>Follow</button>}
-                        </div>
-                    </span>
-                    <span>
-                        <span>
-                            <div>
-                                {user.fullName}
-                            </div>
-                            <div>
-                                {user.status}
-                            </div>
-                        </span>
-                        <span>
-                            <div>
-                                {user.location.country}
-                            </div>
-                            <div>
-                                {user.location.city}
-                            </div>
-                        </span>
-                    </span>
+        return (
+            <span>
+                <div>
+                    <img src={photoUrl} className={styles.Photo} />
                 </div>
-            ))
-        }
-    </div>
+                <div>
+                    {followed
+                        ? <button onClick={() => { props.unfollow(id); }}>Unfollow</button>
+                        : <button onClick={() => { props.follow(id); }}>Follow</button>}
+                </div>
+            </span>
+        );
+    };
+
+    const span2 = (user) => {
+        const { fullName, status } = user;
+        const { country, city } = user.location;
+
+        return (
+            <span>
+                <span>
+                    <div>
+                        {fullName}
+                    </div>
+                    <div>
+                        {status}
+                    </div>
+                </span>
+                <span>
+                    <div>
+                        {country}
+                    </div>
+                    <div>
+                        {city}
+                    </div>
+                </span>
+            </span>
+        )
+    };
+
+    const newMap = props.users.map((user) => {
+        return (
+            <div key={user.id}>
+                {span1(user)}
+                {span2(user)}
+            </div>
+        );
+    });
+
+    return (
+        <div>
+            {newMap}
+        </div>
+    );
 };
 
 export default Users;
+
