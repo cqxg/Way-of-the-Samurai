52a5f490645b5c18080eea4f62ef26f97e4d9c1d
cqxg <cqxg@yandex.ru>
2020-01-18 15:26:19 +0300

    fix: imports/exports

--- src/redux/reducers/profileReducer.js
+++ src/redux/reducers/profileReducer.js
@@ -1,49 +1,50 @@
-import { ADD_POST, UPDATE_NEW_POST_TEXT } from '../actions/actionTypes';
 import { DEFAULT_LIKES_VALUE, NEXT_ID } from '../../utils/constants';
 
-const initialState = {
-  posts: [
-    {
-      id: '1',
-      message: 'Hi, how are you',
-      likesCount: '12',
-    },
-    {
-      id: '2',
-      message: 'It`s my first posts',
-      likesCount: '7',
-    },
-  ],
+import { ADD_POST, UPDATE_NEW_POST_TEXT } from '../actions/actionTypes';
 
-  newPostText: '',
+const initialState = {
+    posts: [
+        {
+            id: '1',
+            message: 'Hi, how are you',
+            likesCount: '12',
+        },
+        {
+            id: '2',
+            message: 'It`s my first posts',
+            likesCount: '7',
+        },
+    ],
+
+    newPostText: '',
 };
 
 const profileReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_POST: {
-      const newPost = {
-        id: NEXT_ID,
-        message: state.newPostText,
-        likesCount: DEFAULT_LIKES_VALUE,
-      };
+    switch (action.type) {
+        case ADD_POST: {
+            const newPost = {
+                id: NEXT_ID,
+                message: state.newPostText,
+                likesCount: DEFAULT_LIKES_VALUE,
+            };
 
-      return {
-        ...state,
-        newPostText: '',
-        posts: [...state.posts, newPost],
-      };
+            return {
+                ...state,
+                newPostText: '',
+                posts: [...state.posts, newPost],
+            };
+        }
+
+        case UPDATE_NEW_POST_TEXT: {
+            return {
+                ...state,
+                newPostText: action.newText,
+            };
+        }
+
+        default:
+            return state;
     }
-
-    case UPDATE_NEW_POST_TEXT: {
-      return {
-        ...state,
-        newPostText: action.newText,
-      };
-    }
-
-    default:
-      return state;
-  }
 };
 
 export { profileReducer };
--- src/redux/reducers/usersReducer.js
+++ src/redux/reducers/usersReducer.js
@@ -1,81 +1,82 @@
-import { FOLLOW, UNFOLLOW, SET_USERS } from '../actions/actionTypes';
 import { AVATAR_DEFAULT } from '../../utils/url-utils';
 
+import { FOLLOW, UNFOLLOW, SET_USERS } from '../actions/actionTypes';
+
 const initialState = {
-  users: [
-    {
-      id: '1',
-      photoUrl: { AVATAR_DEFAULT },
-      followed: true,
-      fullName: 'Vasiliy',
-      status: 'vasiliy propionate',
-      location: {
-        city: 'Giran Harbor',
-        country: 'Elmoraden',
-      },
-    },
-    {
-      id: '2',
-      photoUrl: { AVATAR_DEFAULT },
-      followed: true,
-      fullName: 'Gennadiy',
-      status: 'vse dl9 pacanov',
-      location: {
-        city: 'Vorota Diona',
-        country: 'Dion',
-      },
-    },
-    {
-      id: '3',
-      photoUrl: { AVATAR_DEFAULT },
-      followed: true,
-      fullName: 'Tatiyana',
-      status: 'Tanysha podai grabli!!!1',
-      location: {
-        city: 'Minsk',
-        country: 'Belarus',
-      },
-    },
-  ],
+    users: [
+        {
+            id: '1',
+            photoUrl: { AVATAR_DEFAULT },
+            followed: true,
+            fullName: 'Vasiliy',
+            status: 'vasiliy propionate',
+            location: {
+                city: 'Giran Harbor',
+                country: 'Elmoraden',
+            },
+        },
+        {
+            id: '2',
+            photoUrl: { AVATAR_DEFAULT },
+            followed: true,
+            fullName: 'Gennadiy',
+            status: 'vse dl9 pacanov',
+            location: {
+                city: 'Vorota Diona',
+                country: 'Dion',
+            },
+        },
+        {
+            id: '3',
+            photoUrl: { AVATAR_DEFAULT },
+            followed: true,
+            fullName: 'Tatiyana',
+            status: 'Tanysha podai grabli!!!1',
+            location: {
+                city: 'Minsk',
+                country: 'Belarus',
+            },
+        },
+    ],
 };
 
 const usersReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case FOLLOW:
-      return {
-        ...state,
-        users: state.users.map((user) => {
-          if (user.id === action.userID) {
-            return { ...user, followed: true };
-          }
-          return user;
-        }),
-      };
+    switch (action.type) {
+        case FOLLOW:
+            return {
+                ...state,
+                users: state.users.map((user) => {
+                    if (user.id === action.userID) {
+                        return { ...user, followed: true };
+                    }
+                    return user;
+                }),
+            };
 
-    case UNFOLLOW:
-      return {
-        ...state,
-        users: state.users.map((user) => {
-          if (user.id === action.userID) {
-            return { ...user, followed: false };
-          }
-          return user;
-        }),
-      };
+        case UNFOLLOW:
+            return {
+                ...state,
+                users: state.users.map((user) => {
+                    if (user.id === action.userID) {
+                        return { ...user, followed: false };
+                    }
+                    return user;
+                }),
+            };
 
-    case SET_USERS:
-      return {
-        ...state,
-        users: [...state.users, ...action.users],
-      };
+        case SET_USERS:
+            return {
+                ...state,
+                users: [...state.users, ...action.users],
+            };
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 };
 
-export const followActionCreator = (userID) => ({ type: FOLLOW, userID });
-export const unfollowActionCreator = (userID) => ({ type: UNFOLLOW, userID });
-export const setUsersActionCreator = (users) => ({ type: UNFOLLOW, users });
+const followActionCreator = (userID) => ({ type: FOLLOW, userID });
+const unfollowActionCreator = (userID) => ({ type: UNFOLLOW, userID });
+const setUsersActionCreator = (users) => ({ type: UNFOLLOW, users });
 
-export { usersReducer };
+export { usersReducer, followActionCreator, unfollowActionCreator, setUsersActionCreator };
