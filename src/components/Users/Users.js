34c860dfe6f5ebdcd9e8130911a910977a6baeb3
cqxg <cqxg@yandex.ru>
2020-01-18 14:11:41 +0300

    feat: add axios

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,44 +1,51 @@
 import React from 'react';
 
+// import Axios from 'axios';
+
 import styles from './Users.module.css';
 
-const Users = (props) => (
-  <div>
-    {
-                props.users.map((user) => (
-                  <div key={user.id}>
+const Users = (props) => {
+
+    // axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+    //     props.setUsers()
+    // });
+
+    <div>
+        {
+            props.users.map((user) => (
+                <div key={user.id}>
                     <span>
-                      <div>
-                        <img src={user.photoUrl} className={styles.userPhoto} />
-                      </div>
-                      <div>
-                        {user.followed
-                          ? <button onClick={() => { props.unfollow(user.id); }}>Unfollow</button>
-                          : <button onClick={() => { props.follow(user.id); }}>Follow</button>}
-                      </div>
+                        <div>
+                            <img src={user.photoUrl} className={styles.userPhoto} />
+                        </div>
+                        <div>
+                            {user.followed
+                                ? <button onClick={() => { props.unfollow(user.id); }}>Unfollow</button>
+                                : <button onClick={() => { props.follow(user.id); }}>Follow</button>}
+                        </div>
                     </span>
                     <span>
-                      <span>
-                        <div>
-                          {user.fullName}
-                        </div>
-                        <div>
-                          {user.status}
-                        </div>
-                      </span>
-                      <span>
-                        <div>
-                          {user.location.country}
-                        </div>
-                        <div>
-                          {user.location.city}
-                        </div>
-                      </span>
+                        <span>
+                            <div>
+                                {user.fullName}
+                            </div>
+                            <div>
+                                {user.status}
+                            </div>
+                        </span>
+                        <span>
+                            <div>
+                                {user.location.country}
+                            </div>
+                            <div>
+                                {user.location.city}
+                            </div>
+                        </span>
                     </span>
-                  </div>
-                ))
-}
-  </div>
-);
+                </div>
+            ))
+        }
+    </div>
+};
 
 export default Users;
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,22 +1,23 @@
 import { connect } from 'react-redux';
 
-import Users from './Users';
 import { followActionCreator, unfollowActionCreator, setUsersActionCreator } from '../../redux/reducers/usersReducer';
 
+import Users from './Users';
+
 const mapStateToProps = (state) => ({
-  users: state.usersPage.users,
+    users: state.usersPage.users,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  follow: (userID) => {
-    dispatch(followActionCreator(userID));
-  },
-  unfollow: (userID) => {
-    dispatch(unfollowActionCreator(userID));
-  },
-  setUsers: (users) => {
-    dispatch(setUsersActionCreator(users));
-  },
+    follow: (userID) => {
+        dispatch(followActionCreator(userID));
+    },
+    unfollow: (userID) => {
+        dispatch(unfollowActionCreator(userID));
+    },
+    setUsers: (users) => {
+        dispatch(setUsersActionCreator(users));
+    },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Users);
