dd74e09382f25dcf9a8eaf15d4c070d51ab14aef
cqxg <cqxg@yandex.ru>
2020-01-19 21:04:39 +0300

    fix: add componentDidMount

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -9,6 +9,9 @@ import styles from './Users.module.css';
 class Users extends React.Component {
     constructor(props) {
         super(props);
+    };
+
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items);
