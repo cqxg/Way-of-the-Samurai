8138d622e0d395b4790e904eb4c849f04eb23ea5
cqxg <cqxg@yandex.ru>
2020-01-20 15:34:32 +0300

    fix: preloader moved to utilities

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import axios from 'axios';
+import Preloader from '../../utils/preloader';
 
-import preloader from '../../assets/images/preloader.svg'
+import axios from 'axios';
 
 import {
     followActionCreator,
@@ -41,8 +41,8 @@ class UsersContainer extends React.Component {
     render() {
         return (
             <>
-                {this.props.isFetching ? <img src={preloader} /> : null}
-                <Users totalUsersCount={this.props.totalUsersCount}
+                {this.props.isFetching ? <Preloader /> : null}
+                < Users totalUsersCount={this.props.totalUsersCount}
                     pageSize={this.props.pageSize}
                     currentPage={this.props.currentPage}
                     onPageChanged={this.onPageChanged}
---
+++ src/utils/preloader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import preloader from '../assets/images/preloader.svg';
+
+const Preloader = () => <img src={preloader} />;
+
+export default Preloader;
