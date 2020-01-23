76a873f672a57dfe6ca9cef588f53f418fdfabaf
cqxg <cqxg@yandex.ru>
2020-01-23 13:10:11 +0300

    fix: default avatars

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -3,72 +3,75 @@ import React from 'react';
 import userPhoto from '../../assets/images/unnamed.jpg';
 
 import styles from './Users.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Users = (props) => {
-  const pagination = () => {
-    const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    const pagination = () => {
+        const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-    const pages = [...Array(pagesCount).keys()];
+        const pages = [...Array(pagesCount).keys()];
 
-    const pagesMap = pages.map((page) => (
-      <span
-        className={props.currentPage === page && styles.selectedPage}
-        onClick={() => props.onPageChanged(page)}
-      >
-        {page}
-      </span>
-    ));
+        const pagesMap = pages.map((page) => (
+            <span
+                className={props.currentPage === page && styles.selectedPage}
+                onClick={() => props.onPageChanged(page)}
+            >
+                {page}
+            </span>
+        ));
+
+        return (
+            <div>
+                {pagesMap}
+            </div>
+        );
+    };
+
+    const goMap = () => {
+        const newMap = props.users.map((user) => (
+            <div key={user.id}>
+                {span1(user)}
+                {span2(user)}
+            </div>
+        ));
+
+        return newMap;
+    };
+
+    const span1 = (user) => (
+        <span>
+            <div>
+                <NavLink to={`/profile/${user.id}`}>
+                    <img src={user.photos.small != null ? user.photos.small : userPhoto} className={styles.Photo} />
+                </NavLink>
+            </div>
+            <div>
+                {user.followed
+                    ? <button onClick={() => props.unfollow(user.id)}>Unfollow</button>
+                    : <button onClick={() => props.follow(user.id)}>Follow</button>}
+            </div>
+        </span>
+    );
+
+    const span2 = (user) => (
+        <span>
+            <span>
+                <div>
+                    {user.name}
+                </div>
+                <div>
+                    {user.status}
+                </div>
+            </span>
+        </span>
+    );
 
     return (
-      <div>
-        {pagesMap}
-      </div>
+        <div>
+            {pagination()}
+            {goMap()}
+        </div>
     );
-  };
-
-  const goMap = () => {
-    const newMap = props.users.map((user) => (
-      <div key={user.id}>
-        {span1(user)}
-        {span2(user)}
-      </div>
-    ));
-
-    return newMap;
-  };
-
-  const span1 = (user) => (
-    <span>
-      <div>
-        <img src={user.photos.small != null ? user.photos.small : { userPhoto }} className={styles.Photo} />
-      </div>
-      <div>
-        {user.followed
-          ? <button onClick={() => props.unfollow(user.id)}>Unfollow</button>
-          : <button onClick={() => props.follow(user.id)}>Follow</button>}
-      </div>
-    </span>
-  );
-
-  const span2 = (user) => (
-    <span>
-      <span>
-        <div>
-          {user.name}
-        </div>
-        <div>
-          {user.status}
-        </div>
-      </span>
-    </span>
-  );
-
-  return (
-    <div>
-      {pagination()}
-      {goMap()}
-    </div>
-  );
 };
 
 export default Users;
