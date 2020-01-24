75770465f29822ef1f8c3e4af2c90350594b2dde
cqxg <cqxg@yandex.ru>
2020-01-24 23:40:04 +0300

    fix: imports

--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-
 import axios from 'axios';
 
 import { PROFILE_URL } from '../../utils/url-utils';
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 import axios from 'axios';
 
 import { USERS_URL } from '../../utils/url-utils';
