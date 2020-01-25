51b0d4a5733298fa2726ef3baedc12e3fa20792a
cqxg <cqxg@yandex.ru>
2020-01-25 12:57:45 +0300

    feat: add setUserData action creator workpiece

--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -1,29 +1,30 @@
 import {
-  ADD_POST,
-  UPDATE_NEW_POST_TEXT,
-  UPDATE_NEW_MESSAGE_BODY,
-  SEND_MESSAGE,
-  FOLLOW,
-  UNFOLLOW,
-  SET_USERS,
-  SET_CURRENT_PAGE,
-  SET_TOTAL_USERS_COUNT,
-  TOGGLE_IS_FETCHING,
-  SET_USER_PROFILE,
+    ADD_POST,
+    UPDATE_NEW_POST_TEXT,
+    UPDATE_NEW_MESSAGE_BODY,
+    SEND_MESSAGE,
+    FOLLOW,
+    UNFOLLOW,
+    SET_USERS,
+    SET_CURRENT_PAGE,
+    SET_TOTAL_USERS_COUNT,
+    TOGGLE_IS_FETCHING,
+    SET_USER_PROFILE,
+    SET_USER_DATA
 } from './actionTypes';
 
 const sendMessageCreator = () => ({ type: SEND_MESSAGE });
 
 const updateNewMessageBodyCreator = (body) => ({
-  type: UPDATE_NEW_MESSAGE_BODY,
-  body,
+    type: UPDATE_NEW_MESSAGE_BODY,
+    body,
 });
 
 const addPostActionCreator = () => ({ type: ADD_POST });
 
 const updateNewPostTextActionCreator = (text) => ({
-  type: UPDATE_NEW_POST_TEXT,
-  newText: text,
+    type: UPDATE_NEW_POST_TEXT,
+    newText: text,
 });
 
 const follow = (userID) => ({ type: FOLLOW, userID });
@@ -33,17 +34,19 @@ const setCurrentPage = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage }
 const setTotalUsersCount = (totalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, totalUsersCount });
 const toggleIsFetching = (isFetching) => ({ type: TOGGLE_IS_FETCHING, isFetching });
 const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile });
+const setUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: { userId, email, login } });
 
 export {
-  follow,
-  unfollow,
-  setUsers,
-  sendMessageCreator,
-  updateNewMessageBodyCreator,
-  addPostActionCreator,
-  updateNewPostTextActionCreator,
-  setCurrentPage,
-  setTotalUsersCount,
-  toggleIsFetching,
-  setUserProfile,
+    follow,
+    unfollow,
+    setUsers,
+    sendMessageCreator,
+    updateNewMessageBodyCreator,
+    addPostActionCreator,
+    updateNewPostTextActionCreator,
+    setCurrentPage,
+    setTotalUsersCount,
+    toggleIsFetching,
+    setUserProfile,
+    setUserData
 };
