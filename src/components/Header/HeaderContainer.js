7de57ce91dc6000410012aa62fb7729286301334
cqxg <cqxg@yandex.ru>
2020-01-28 21:19:58 +0300

    feat: add request preparation in header container

--- src/components/Header/HeaderContainer.js
+++ src/components/Header/HeaderContainer.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Header from './Header';
 
 class HeaderContainer extends Component() {
-    render () {
-        return <Header />
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
+            withCredentials: true
+        })
+            .then(response => {
+                debugger
+            });
     }
-}
+
+    render() {
+        return <Header {...this.props} />
+    };
+};
+
 export default HeaderContainer;
