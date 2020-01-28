3c3eee65de74ad0927da90331caac9d1fcf001f2
cqxg <cqxg@yandex.ru>
2020-01-28 21:25:36 +0300

    feat: add mapStateToProps for header container

--- src/components/Header/HeaderContainer.js
+++ src/components/Header/HeaderContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
 
 import Header from './Header';
@@ -19,4 +20,8 @@ class HeaderContainer extends Component() {
     };
 };
 
-export default HeaderContainer;
+const mapStateToProps = () => {{
+    
+}}
+
+export default connect () (HeaderContainer);
