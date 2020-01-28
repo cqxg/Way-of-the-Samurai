85f7b8d0571b3d1b71ab45dead1b18a87d758080
cqxg <cqxg@yandex.ru>
2020-01-28 21:07:28 +0300

    feat: add HeaderContainer workpiece

---
+++ src/components/Header/HeaderContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+import Header from './Header';
+
+class HeaderContainer extends Component() {
+    render () {
+        return <Header />
+    }
+}
+export default HeaderContainer;
