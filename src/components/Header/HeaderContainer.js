472d4b3d7df3d93b706440cd6aa24c0c79d7fde9
cqxg <cqxg@yandex.ru>
2020-01-31 00:30:43 +0300

    fix: found damn authorization error

--- src/components/Header/HeaderContainer.js
+++ src/components/Header/HeaderContainer.js
@@ -2,32 +2,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 
-import { USERS_URL } from '../../utils/url-utils';
+import { PROFILE_URL } from '../../utils/url-utils';
 import { setAuthUserData } from '../../redux/actions/actionCreators';
 
 import Header from './Header';
 
 class HeaderContainer extends Component {
-    componentDidMount() {
-        axios.get(`${USERS_URL}auth/me`, {
-            withCredentials: true,
-        })
-            .then((response) => {
-                if (response.data.resultCode === 0) {
-                    const { id, email, login } = response.data.data;
-                    this.props.setAuthUserData(id, email, login);
-                }
-            });
-    }
+  componentDidMount() {
+    axios.get(`${PROFILE_URL}auth/me`, {
+      withCredentials: true,
+    })
+      .then((response) => {
+        if (response.data.resultCode === 0) {
+          const { id, email, login } = response.data.data;
+          this.props.setAuthUserData(id, email, login);
+        }
+      });
+  }
 
-    render() {
-        return <Header {...this.props} />;
-    }
+  render() {
+    return <Header {...this.props} />;
+  }
 }
 
 const mapStateToProps = (state) => ({
-    isAuth: state.auth.isAuth,
-    login: state.auth.login,
+  isAuth: state.auth.isAuth,
+  login: state.auth.login,
 });
 
 export default connect(mapStateToProps, { setAuthUserData })(HeaderContainer);
