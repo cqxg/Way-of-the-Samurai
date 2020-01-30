c16173c775f9ca4cdba503e6f73becce6fc0b264
cqxg <cqxg@yandex.ru>
2020-01-30 22:19:43 +0300

    feat: add preparing follow/ unfollow logic

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -6,72 +6,92 @@ import userPhoto from '../../assets/images/unnamed.jpg';
 import styles from './Users.module.css';
 
 const Users = (props) => {
-  const pagination = () => {
-    const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    const pagination = () => {
+        const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-    const pages = [...Array(pagesCount).keys()];
+        const pages = [...Array(pagesCount).keys()];
 
-    const pagesMap = pages.map((page) => (
-      <span
-        className={props.currentPage === page && styles.selectedPage}
-        onClick={() => props.onPageChanged(page)}
-      >
-        {page}
-      </span>
-    ));
+        const pagesMap = pages.map((page) => (
+            <span
+                className={props.currentPage === page && styles.selectedPage}
+                onClick={() => props.onPageChanged(page)}
+            >
+                {page}
+            </span>
+        ));
+
+        return (
+            <div>
+                {pagesMap}
+            </div>
+        );
+    };
+
+    const goMap = () => {
+        const newMap = props.users.map((user) => (
+            <div key={user.id}>
+                {span1(user)}
+                {span2(user)}
+            </div>
+        ));
+
+        return newMap;
+    };
+
+    const span1 = (user) => {
+        <span>
+            <div>
+                <NavLink to={`/profile/${user.id}`}>
+                    <img src={user.photos.small != null ? user.photos.small : userPhoto} className={styles.Photo} />
+                </NavLink>
+            </div>
+            <div>
+                {user.followed
+
+                    ? <button onClick={() => {
+                        axios.post(`${USERS_URL}follow/${userid}`, {}, {
+                            withCredentials: true,
+                        })
+                            .then(response => {
+                                if (response.data.resultCode === 0) {
+                                    props.unfollow(user.id)
+                                }
+                            });
+                    }}>Unfollow</button>
+
+                    : <button onClick={() => {
+                        axios.post(`${USERS_URL}follow/${userid}`, {}, {
+                            withCredentials: true,
+                        })
+                            .then(response => {
+                                if (response.data.resultCode === 0) {
+                                    props.follow(user.id)
+                                }
+                            });
+                    }}>Follow</button>}
+            </div>
+        </span>
+    };
+
+    const span2 = (user) => (
+        <span>
+            <span>
+                <div>
+                    {user.name}
+                </div>
+                <div>
+                    {user.status}
+                </div>
+            </span>
+        </span>
+    );
 
     return (
-      <div>
-        {pagesMap}
-      </div>
+        <div>
+            {pagination()}
+            {goMap()}
+        </div>
     );
-  };
-
-  const goMap = () => {
-    const newMap = props.users.map((user) => (
-      <div key={user.id}>
-        {span1(user)}
-        {span2(user)}
-      </div>
-    ));
-
-    return newMap;
-  };
-
-  const span1 = (user) => (
-    <span>
-      <div>
-        <NavLink to={`/profile/${user.id}`}>
-          <img src={user.photos.small != null ? user.photos.small : userPhoto} className={styles.Photo} />
-        </NavLink>
-      </div>
-      <div>
-        {user.followed
-          ? <button onClick={() => props.unfollow(user.id)}>Unfollow</button>
-          : <button onClick={() => props.follow(user.id)}>Follow</button>}
-      </div>
-    </span>
-  );
-
-  const span2 = (user) => (
-    <span>
-      <span>
-        <div>
-          {user.name}
-        </div>
-        <div>
-          {user.status}
-        </div>
-      </span>
-    </span>
-  );
-
-  return (
-    <div>
-      {pagination()}
-      {goMap()}
-    </div>
-  );
 };
 
 export default Users;
