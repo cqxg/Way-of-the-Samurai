ae98bd6f2a832029d3aa285f29d6748a68760259
cqxg <cqxg@yandex.ru>
2020-02-03 22:58:39 +0300

    feat: add props validation in profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import MyPostsContainer from './MyPosts/MyPostsContainer';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
@@ -11,6 +12,14 @@ const Profile = (props) => {
       <MyPostsContainer />
     </div>
   );
+};
+
+Profile.defaultProps = {
+  profile: PropTypes.number,
+};
+
+Profile.propTypes = {
+  profile: PropTypes.number,
 };
 
 export default Profile;
