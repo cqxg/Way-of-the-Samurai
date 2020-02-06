da4663a8d655aaa7c48fede909a6848fe2700fa2
Ihar Ivaniuk <cqxg@yandex.ru>
2020-02-06 22:56:18 +0300

    feat: add axios instance

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 import { MAIN_URL } from '../utils/url-utils';
 
+const instance = axios.create();
+
 const getUsers = (pageNumber, pageSize) => axios.get(`${MAIN_URL}users?page=${pageNumber}&count=${pageSize}`,
   {
     withCredentials: true,
