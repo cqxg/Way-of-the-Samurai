cb9b2dd4cd182a3b2f992118f690a08eeb99d309
Ihar Ivaniuk <cqxg@yandex.ru>
2020-02-06 22:58:30 +0300

    feat: add credentials in axios instance

--- src/api/api.js
+++ src/api/api.js
@@ -2,12 +2,16 @@ import axios from 'axios';
 
 import { MAIN_URL } from '../utils/url-utils';
 
-const instance = axios.create();
+const instance = axios.create(
+    {
+        withCredentials: true,
+    }
+);
 
 const getUsers = (pageNumber, pageSize) => axios.get(`${MAIN_URL}users?page=${pageNumber}&count=${pageSize}`,
-  {
-    withCredentials: true,
-  })
-  .then((response) => response.data);
+    {
+        withCredentials: true,
+    })
+    .then((response) => response.data);
 
 export default getUsers;
