03714f0e31b446e2132ae5daba99f38509331930
Ihar Ivaniuk <cqxg@yandex.ru>
2020-02-07 23:09:00 +0300

    feat: add headers in axios instance

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
 import { MAIN_URL } from '../utils/url-utils';
+import { API_KEY } from '../utils/constants';
 
 const instance = axios.create(
     {
         withCredentials: true,
         baseURL: MAIN_URL,
+        headers: {
+            'API-KEY': API_KEY,
+          },
     }
 );
 
