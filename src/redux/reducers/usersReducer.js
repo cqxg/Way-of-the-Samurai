38a1d172480a12f8aaebf2018e46d130afa42caf
Ihar Ivaniuk <cqxg@yandex.ru>
2020-02-12 22:21:09 +0300

    feat: add followingInProgress will be an array

--- src/redux/reducers/usersReducer.js
+++ src/redux/reducers/usersReducer.js
@@ -14,7 +14,7 @@ const initialState = {
   totalUsersCount: 0,
   currentPage: 1,
   isFetching: false,
-  followingInProgress: false,
+  followingInProgress: [],
 };
 
 const usersReducer = (state = initialState, action) => {
