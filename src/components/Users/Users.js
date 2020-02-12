3d7910b39f6ec2626e5acbac18c89bd95f846e15
Ihar Ivaniuk <cqxg@yandex.ru>
2020-02-12 22:18:20 +0300

    fix: completely fixed follow/unfollow disabling

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -11,7 +11,12 @@ import styles from './Users.module.css';
 
 const Users = (props) => {
   const {
-    totalUsersCount, pageSize, currentPage, onPageChanged, toggleFollowingProgress,
+    totalUsersCount,
+    pageSize,
+    currentPage,
+    onPageChanged,
+    toggleFollowingProgress,
+    followingInProgress,
   } = props;
   const pagination = () => {
     const pagesCount = Math.ceil(totalUsersCount / pageSize);
@@ -84,8 +89,8 @@ const Users = (props) => {
       </div>
       <div>
         {user.followed
-          ? <button disabled={toggleFollowingProgress} type="submit" onClick={() => goUnfollow(user)}>Unfollow</button>
-          : <button disabled={toggleFollowingProgress} type="submit" onClick={() => goFollow(user)}>Follow</button>}
+          ? <button disabled={followingInProgress} type="submit" onClick={() => goUnfollow(user)}>Unfollow</button>
+          : <button disabled={followingInProgress} type="submit" onClick={() => goFollow(user)}>Follow</button>}
       </div>
     </span>
   );
@@ -131,6 +136,7 @@ Users.defaultProps = {
   unfollow: PropTypes.func,
   onPageChanged: PropTypes.func,
   toggleFollowingProgress: PropTypes.func,
+  followingInProgress: PropTypes.bool,
 };
 
 Users.propTypes = {
@@ -142,6 +148,7 @@ Users.propTypes = {
   unfollow: PropTypes.func,
   onPageChanged: PropTypes.func,
   toggleFollowingProgress: PropTypes.func,
+  followingInProgress: PropTypes.bool,
 };
 
 export default Users;
