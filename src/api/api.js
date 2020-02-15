84f73c954a4cf3e3a7b7a9e70caceb2862338ea3
Ihar Ivaniuk <cqxg@yandex.ru>
2020-02-15 14:40:06 +0300

    feat: add unfollow method workpiece in API

--- src/api/api.js
+++ src/api/api.js
@@ -2,25 +2,30 @@ import axios from 'axios';
 
 import { MAIN_URL } from '../utils/url-utils';
 import { API_KEY } from '../utils/constants';
+import { unfollow } from '../redux/actions/actionCreators';
 
 const instance = axios.create(
-  {
-    withCredentials: true,
-    baseURL: MAIN_URL,
-    headers: {
-      'API-KEY': API_KEY,
+    {
+        withCredentials: true,
+        baseURL: MAIN_URL,
+        headers: {
+            'API-KEY': API_KEY,
+        },
     },
-  },
 );
 
 const usersAPI = {
-  getUsers(currentPage, pageSize) {
-    return instance.get(`users?page=${currentPage}&count=${pageSize}`).then((response) => response.data);
-  },
+    getUsers(currentPage, pageSize) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then((response) => response.data);
+    },
 
-  follow(userId) {
+    follow(userId) {
 
-  }
+    },
+
+    unfollow(userId) {
+
+    }
 };
 
 export default usersAPI;
