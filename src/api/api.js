fdb243bd23b5a34f06bf8f90d96f08eb06ea1c38
Ihar Ivaniuk <cqxg@yandex.ru>
2020-02-15 14:28:03 +0300

    fix: getPage removed(logic combined)

--- src/api/api.js
+++ src/api/api.js
@@ -16,11 +16,6 @@ const instance = axios.create(
 const usersAPI = {
   getUsers(currentPage, pageSize) {
     return instance.get(`users?page=${currentPage}&count=${pageSize}`).then((response) => response.data);
-  },
-
-  getPage(pageNumber, pageSize) {
-    return instance.get(`users?page=${pageNumber}&count=${pageSize}`).then((response) => response.data);
-  },
-};
+  }};
 
 export default usersAPI;
