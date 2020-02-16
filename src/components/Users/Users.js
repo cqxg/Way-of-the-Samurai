1a56600b96a9e988950c1b81c082006ebac0966c
Ihar Ivaniuk <cqxg@yandex.ru>
2020-02-16 21:31:04 +0300

    feat: follow button logic transferred

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -8,134 +8,130 @@ import { ALT_USER_AVATAR } from '../../utils/constants';
 import styles from './Users.module.css';
 
 const Users = (props) => {
-  const {
-    follow,
-    unfollow,
-    pageSize,
-    currentPage,
-    onPageChanged,
-    totalUsersCount,
-    followingInProgress,
-  } = props;
-  const pagination = () => {
-    const pagesCount = Math.ceil(totalUsersCount / pageSize);
+    const {
+        follow,
+        unfollow,
+        pageSize,
+        currentPage,
+        onPageChanged,
+        totalUsersCount,
+        followingInProgress,
+    } = props;
+    const pagination = () => {
+        const pagesCount = Math.ceil(totalUsersCount / pageSize);
 
-    const pages = [...Array(pagesCount).keys()];
+        const pages = [...Array(pagesCount).keys()];
 
-    const pagesMap = pages.map((page) => (
-      <span
-        role="button"
-        tabIndex={0}
-        className={currentPage === page && styles.selectedPage}
-        onClick={() => onPageChanged(page)}
-        onKeyPress={() => onPageChanged(page)}
-      >
-        {page}
-      </span>
-    ));
+        const pagesMap = pages.map((page) => (
+            <span
+                role="button"
+                tabIndex={0}
+                className={currentPage === page && styles.selectedPage}
+                onClick={() => onPageChanged(page)}
+                onKeyPress={() => onPageChanged(page)}
+            >
+                {page}
+            </span>
+        ));
+
+        return (
+            <div>
+                {pagesMap}
+            </div>
+        );
+    };
+
+    const followBtn = (user) => {
+        return (
+        <button type="submit" disabled={followingInProgress.some((id) => id === user.id)} onClick={() => follow(user.id)}>
+                {' '}
+                Follow
+        </button>
+        )
+    };
+
+    const span1 = (user) => (
+        <span>
+            <div>
+                <NavLink to={`/profile/${user.id}`}>
+                    <img
+                        alt={ALT_USER_AVATAR}
+                        src={user.photos.small != null
+                            ? user.photos.small
+                            : userPhoto}
+                        className={styles.Photo}
+                    />
+                </NavLink>
+            </div>
+            <div>
+                {user.followed
+                    ? (
+                        <button
+                            type="submit"
+                            disabled={followingInProgress.some((id) => id === user.id)}
+                            onClick={() => unfollow(user.id)}
+                        >
+                            {' '}
+                            Unfollow
+            </button>
+                    )
+                    : followBtn(user)}
+            </div>
+        </span>
+    );
+
+    const span2 = (user) => (
+        <span>
+            <span>
+                <div>
+                    {user.name}
+                </div>
+                <div>
+                    {user.status}
+                </div>
+            </span>
+        </span>
+    );
+
+    const goMap = () => {
+        const newMap = props.users.map((user) => (
+            <div key={user.id}>
+                {span1(user)}
+                {span2(user)}
+            </div>
+        ));
+
+        return newMap;
+    };
 
     return (
-      <div>
-        {pagesMap}
-      </div>
+        <div>
+            {pagination()}
+            {goMap()}
+        </div>
     );
-  };
-  
-  const followBtn = () => {
-
-  };
-
-  const span1 = (user) => (
-    <span>
-      <div>
-        <NavLink to={`/profile/${user.id}`}>
-          <img
-            alt={ALT_USER_AVATAR}
-            src={user.photos.small != null
-              ? user.photos.small
-              : userPhoto}
-            className={styles.Photo}
-          />
-        </NavLink>
-      </div>
-      <div>
-        {user.followed
-          ? (
-            <button
-              type="submit"
-              disabled={followingInProgress.some((id) => id === user.id)}
-              onClick={() => unfollow(user.id)}
-            >
-              {' '}
-Unfollow
-            </button>
-          )
-          : (
-            <button
-              type="submit"
-              disabled={followingInProgress.some((id) => id === user.id)}
-              onClick={() => follow(user.id)}
-            >
-              {' '}
-Follow
-            </button>
-          )}
-      </div>
-    </span>
-  );
-
-  const span2 = (user) => (
-    <span>
-      <span>
-        <div>
-          {user.name}
-        </div>
-        <div>
-          {user.status}
-        </div>
-      </span>
-    </span>
-  );
-
-  const goMap = () => {
-    const newMap = props.users.map((user) => (
-      <div key={user.id}>
-        {span1(user)}
-        {span2(user)}
-      </div>
-    ));
-
-    return newMap;
-  };
-
-  return (
-    <div>
-      {pagination()}
-      {goMap()}
-    </div>
-  );
 };
 
 Users.defaultProps = {
-  follow: PropTypes.func,
-  unfollow: PropTypes.func,
-  pageSize: PropTypes.number,
-  onPageChanged: PropTypes.func,
-  currentPage: PropTypes.number,
-  totalUsersCount: PropTypes.number,
-  followingInProgress: PropTypes.bool,
-  users: PropTypes.instanceOf(Array),
+    follow: PropTypes.func,
+    unfollow: PropTypes.func,
+    pageSize: PropTypes.number,
+    onPageChanged: PropTypes.func,
+    currentPage: PropTypes.number,
+    totalUsersCount: PropTypes.number,
+    followingInProgress: PropTypes.bool,
+    users: PropTypes.instanceOf(Array),
 };
 
 Users.propTypes = {
-  follow: PropTypes.func,
-  unfollow: PropTypes.func,
-  pageSize: PropTypes.number,
-  onPageChanged: PropTypes.func,
-  currentPage: PropTypes.number,
-  totalUsersCount: PropTypes.number,
-  followingInProgress: PropTypes.bool,
-  users: PropTypes.instanceOf(Array),
+    follow: PropTypes.func,
+    unfollow: PropTypes.func,
+    pageSize: PropTypes.number,
+    onPageChanged: PropTypes.func,
+    currentPage: PropTypes.number,
+    totalUsersCount: PropTypes.number,
+    followingInProgress: PropTypes.bool,
+    users: PropTypes.instanceOf(Array),
 };
 
 export default Users;
