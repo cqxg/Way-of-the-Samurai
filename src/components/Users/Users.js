de92e51b3744efeb0e24d13156ac6aed94a3b09e
Ihar Ivaniuk <cqxg@yandex.ru>
2020-02-19 22:37:16 +0300

    fix: received  for a non-boolean attribute in Users

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -23,17 +23,20 @@ const Users = (props) => {
 
     const pages = [...Array(pagesCount).keys()];
 
-    const pagesMap = pages.map((page) => (
-      <span key={page}
-        role="button"
-        tabIndex={0}
-        className={currentPage === page && styles.selectedPage}
-        onClick={() => onPageChanged(page)}
-        onKeyPress={() => onPageChanged(page)}
-      >
-        {page}
-      </span>
-    ));
+    const pagesMap = pages.map((page) => {
+        const isCurrent = currentPage === page;
+        return (
+            <span key={page}
+            role="button"
+            tabIndex={0}
+            className={isCurrent ? styles.selectedPage : null}
+            onClick={() => onPageChanged(page)}
+            onKeyPress={() => onPageChanged(page)}
+          >
+            {page}
+          </span>
+        )
+    });
 
     return (
       <div>
