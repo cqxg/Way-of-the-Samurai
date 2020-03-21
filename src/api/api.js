6f89b60cfb217f483a8fd7df41930c82f3d7b108
Ihar Ivaniuk <cqxg@yandex.ru>
2020-03-21 21:18:39 +0300

    feat: add getStatus in api

--- src/api/api.js
+++ src/api/api.js
@@ -4,42 +4,42 @@ import { MAIN_URL } from '../utils/url-utils';
 import { API_KEY } from '../utils/constants';
 
 const instance = axios.create(
-  {
-    withCredentials: true,
-    baseURL: MAIN_URL,
-    headers: {
-      'API-KEY': API_KEY,
+    {
+        withCredentials: true,
+        baseURL: MAIN_URL,
+        headers: {
+            'API-KEY': API_KEY,
+        },
     },
-  },
 );
 
 const usersAPI = {
-  getUsers(currentPage, pageSize) {
-    return instance.get(`users?page=${currentPage}&count=${pageSize}`).then((response) => response.data);
-  },
+    getUsers(currentPage, pageSize) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then((response) => response.data);
+    },
 
-  follow(userId) {
-    return instance.post(`follow/${userId}`);
-  },
+    follow(userId) {
+        return instance.post(`follow/${userId}`);
+    },
 
-  unfollow(userId) {
-    return instance.delete(`follow/${userId}`);
-  },
+    unfollow(userId) {
+        return instance.delete(`follow/${userId}`);
+    },
 
-  getProfile(userId) {
-    return instance.get(`profile/${userId}`);
-  },
+    getProfile(userId) {
+        return instance.get(`profile/${userId}`);
+    },
 
-  getStatus(userId) {
-    return instance.get(`status/${userId}`);
-  }
+    getStatus(userId) {
+        return instance.get(`status/${userId}`);
+    }
 
 };
 
 const authAPI = {
-  me() {
-    return instance.get('auth/me');
-  },
+    me() {
+        return instance.get('auth/me');
+    },
 };
 
 export { usersAPI, authAPI };
