4a417f5559a7836ce4a724430808c5c25b062cbd
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-02 18:58:39 +0300

    feat: login page rework started

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const Login = () => <h1>LOGIN</h1>;
+const Login = () => {
+    return (
+        <div>
+            <h1>LOGIN</h1>
+        </div>
+    )
+};
 
 export default Login;
