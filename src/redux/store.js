50e1233aa4348a7971bc06abcf2a8ac3cab95c5f
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-02 19:14:44 +0300

    feat: add redux formReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
 
 import profileReducer from './reducers/profileReducer';
 import dialogsReducer from './reducers/dialogsReducer';
@@ -8,11 +9,12 @@ import usersReducer from './reducers/usersReducer';
 import authReducer from './reducers/authReducer';
 
 const reducers = combineReducers({
-  profilePage: profileReducer,
-  dialogsPage: dialogsReducer,
-  usersPage: usersReducer,
-  sidebar: sidebarReducer,
-  auth: authReducer,
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    usersPage: usersReducer,
+    sidebar: sidebarReducer,
+    auth: authReducer,
+    from: formReducer,
 });
 
 const store = createStore(reducers, applyMiddleware(thunkMiddleware));
