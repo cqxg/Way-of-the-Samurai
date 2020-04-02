23936ae7526205b46707d9e669006134c9f55158
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-02 19:06:08 +0300

    feat: add LoginForm component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,21 +4,28 @@ const Login = () => {
     return (
         <div>
             <h1>LOGIN</h1>
-            <form>
-                <div>
-                    <input placeholder='login' />
-                </div>
-                <div>
-                    <input placeholder='password' />
-                </div>
-                <div>
-                    <input type='checkbox' /> remember me
-                </div>
-                <div>
-                    <button>Login</button>
-                </div>
-            </form>
+            <LoginForm />
         </div>
+    )
+};
+
+
+const LoginForm = () => {
+    return (
+        <form>
+            <div>
+                <input placeholder='login' />
+            </div>
+            <div>
+                <input placeholder='password' />
+            </div>
+            <div>
+                <input type='checkbox' /> remember me
+                </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
     )
 };
 
