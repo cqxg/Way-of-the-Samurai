7ecd2d1bacd4701767f911932638a6ca65ea60ff
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-02 19:42:38 +0300

    feat: add onSubmit workpiece in Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,10 +3,15 @@ import React from 'react';
 import LoginReduxForm from './LoginForm';
 
 const Login = () => {
+
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+
     return (
         <div>
             <h1>LOGIN</h1>
-            <LoginReduxForm />
+            <LoginReduxForm onSubmit={onSubmit} />
         </div>
     )
 };
