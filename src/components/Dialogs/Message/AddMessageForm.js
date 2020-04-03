1a3a68cca5f99c2af3269965bcd3ab333e589070
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-03 20:30:26 +0300

    feat: add Field(redux-form) in AddMessageForm

--- src/components/Dialogs/Message/AddMessageForm.js
+++ src/components/Dialogs/Message/AddMessageForm.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Field } from 'redux-form';
 
 const AddMessageForm = (props) => {
     return (
+        <Field component={'textarea'}/>
         <form onSubmit={props.handleSubmit}>
-        <textarea
-          value={state.newMessageBody}
-          onChange={onNewMessageChange}
-          placeholder="Add u message"
-        />
-      </form>
+            <textarea
+                value={state.newMessageBody}
+                onChange={onNewMessageChange}
+                placeholder="Add u message"
+            />
+        </form>
     )
-  };
+};
 
-  export default AddMessageForm;
+export default AddMessageForm;
