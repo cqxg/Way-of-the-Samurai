940301d1a789cd9e45e75ef99aca2f05f2a669ef
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-07 23:28:12 +0300

    feat: validation in Profile page

--- src/utils/FormControl/formsControls.js
+++ src/utils/FormControl/formsControls.js
@@ -2,11 +2,18 @@ import React from 'react';
 
 import styles from './formsControls.module.css';
 
-const Textarea = ({input, meta, ...props}) => {
+const Textarea = ({ input, meta, ...props }) => {
+    const hasError = meta.touched && meta.error;
+
     return (
-        <div className={styles.formControl + ' ' + styles.error}>
-            <textarea {...input} {...props} />
-        </div>
+        <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
+            <div>
+                <textarea {...input} {...props} />
+            </div>
+            <div>
+                {hasError && <span>{meta.error}</span>}
+            </div>
+        </div >
     )
 };
 
