71dcb7194c0479f821fc088ee861089cc883d0d9
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-07 23:54:09 +0300

    feat: add simple validation in Messages page

--- src/components/Dialogs/Message/AddMessageForm.js
+++ src/components/Dialogs/Message/AddMessageForm.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+import { required, maxLengthCreator } from '../../../utils/validators';
+import { Textarea } from '../../../utils/FormControl/formsControls';
 import { I18N } from '../../../utils/constants';
 
+const maxLenght10 = maxLengthCreator(10);
+
 const AddMessageForm = (props) => {
-  const { handleSubmit } = props;
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <Field component="textarea" name="newMessageBody" />
-      </div>
-      <div>
-        <button>{I18N.EN.SEND}</button>
-      </div>
-    </form>
-  );
+    const { handleSubmit } = props;
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <Field component={Textarea} name="newMessageBody" validate={[required, maxLenght10]} />
+            </div>
+            <div>
+                <button>{I18N.EN.SEND}</button>
+            </div>
+        </form>
+    );
 };
 
 const AddMessageFormRedux = reduxForm({ form: 'dialogAddMessageForm' })(AddMessageForm);
