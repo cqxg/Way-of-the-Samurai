7993b52946190fd28a6e9b316815de18950aabf2
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-08 21:40:52 +0300

    feat: add authMe const

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -3,11 +3,13 @@ const count = '&count=';
 const follow = 'follow/';
 const profile = 'profile/';
 const profileStatus = 'profile/status/';
+const authMe = 'auth/me';
 
 export {
     usersPage,
     count,
     follow,
     profile,
-    profileStatus
+    profileStatus,
+    authMe
 }
