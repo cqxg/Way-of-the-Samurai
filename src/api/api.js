d83c49632088d8585e7ef4028cf84c70063d8090
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-08 21:46:08 +0300

    feat: used routes const in api

--- src/api/api.js
+++ src/api/api.js
@@ -2,49 +2,56 @@ import axios from 'axios';
 
 import { MAIN_URL } from '../utils/url-utils';
 import { API_KEY } from '../utils/constants';
-import { usersPage } from '../utils/routes';
+import {
+    count,
+    authMe,
+    follow,
+    profile,
+    usersPage,
+    profileStatus,
+} from '../utils/routes';
 
 const instance = axios.create(
-  {
-    withCredentials: true,
-    baseURL: MAIN_URL,
-    headers: {
-      'API-KEY': API_KEY,
+    {
+        withCredentials: true,
+        baseURL: MAIN_URL,
+        headers: {
+            'API-KEY': API_KEY,
+        },
     },
-  },
 );
 
 const usersAPI = {
-  getUsers(currentPage, pageSize) {
-    return instance.get(`${usersPage}${currentPage}&count=${pageSize}`).then((response) => response.data);
-  },
+    getUsers(currentPage, pageSize) {
+        return instance.get(`${usersPage}${currentPage}${count}${pageSize}`).then((response) => response.data);
+    },
 
-  follow(userId) {
-    return instance.post(`follow/${userId}`);
-  },
+    follow(userId) {
+        return instance.post(`${follow}${userId}`);
+    },
 
-  unfollow(userId) {
-    return instance.delete(`follow/${userId}`);
-  },
+    unfollow(userId) {
+        return instance.delete(`${follow}${userId}`);
+    },
 
-  getProfile(userId) {
-    return instance.get(`profile/${userId}`);
-  },
+    getProfile(userId) {
+        return instance.get(`${profile}${userId}`);
+    },
 
-  getStatus(userId) {
-    return instance.get(`profile/status/${userId}`);
-  },
+    getStatus(userId) {
+        return instance.get(`${profileStatus}${userId}`);
+    },
 
-  updateStatus(status) {
-    return instance.put('profile/status', { status });
-  },
+    updateStatus(status) {
+        return instance.put(profileStatus, { status });
+    },
 
 };
 
 const authAPI = {
-  me() {
-    return instance.get('auth/me');
-  },
+    me() {
+        return instance.get(authMe);
+    },
 };
 
 export { usersAPI, authAPI };
