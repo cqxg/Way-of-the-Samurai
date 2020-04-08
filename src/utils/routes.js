02ed3a6e5e1653ddf29556b038a415e6ccb7133c
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-08 21:39:53 +0300

    feat: add profileStatus const

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -2,10 +2,12 @@ const usersPage = 'users?page=';
 const count = '&count=';
 const follow = 'follow/';
 const profile = 'profile/';
-
+const profileStatus = 'profile/status/';
 
 export {
     usersPage,
     count,
     follow,
+    profile,
+    profileStatus
 }
