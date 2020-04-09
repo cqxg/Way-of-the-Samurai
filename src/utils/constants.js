35ff510a729414d2a173dbb0579ea3beba9d7daa
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-09 22:23:19 +0300

    feat: add I18N workpiece

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,6 +5,15 @@ const API_KEY = '97590639-9a46-4b9d-a8e4-899e90470cbc';
 const ALT_PROFILE_AVATAR = 'Avatar';
 const ALT_USER_AVATAR = 'User Avatar';
 const WELL = 0;
+const I18N = {
+    EN: {
+        
+    },
+
+    RU: {
+
+    }
+}
 
 export {
   DEFAULT_LIKES_VALUE, NEXT_ID, DEFAULT_USER_ID, API_KEY, ALT_PROFILE_AVATAR, ALT_USER_AVATAR, WELL,
