3d47659a7800a4719f92dcc1515ab3dfbddcdb22
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-09 20:06:10 +0300

    feat: routes moved from routes.js in url-utils

--- src/utils/url-utils.js
+++ src/utils/url-utils.js
@@ -1,12 +1,18 @@
 const BACKGROUND_IMG = 'https://a.d-cd.net/870b45as-960.jpg';
 const AVATAR_DEFAULT = 'https://yt3.ggpht.com/a/AGF-l79zTH00bozogFe1oAjQyY7-lnxI-uZMKgonrw=s900-mo-c-c0xffffffff-rj-k-no';
 const MAIN_URL = 'https://social-network.samuraijs.com/api/1.0/';
-const routes = {
-
+const ROUTES = {
+    USERS_PAGE: 'users?page=',
+    COUNT: '&count=',
+    FOLLOW: 'follow/',
+    PROFILE: 'profile/',
+    PROFILE_STATUS: 'profile/status/',
+    AUTH_ME: 'auth/me',
 };
 
 export {
-  BACKGROUND_IMG,
-  AVATAR_DEFAULT,
-  MAIN_URL,
+    BACKGROUND_IMG,
+    AVATAR_DEFAULT,
+    MAIN_URL,
+    ROUTES
 };
