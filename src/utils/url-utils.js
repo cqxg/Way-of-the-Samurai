46d181120fbe5591ba9e8555a8a69ce8e93e9969
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-09 19:57:31 +0300

    feat: add routes const in url-utils

--- src/utils/url-utils.js
+++ src/utils/url-utils.js
@@ -1,6 +1,9 @@
 const BACKGROUND_IMG = 'https://a.d-cd.net/870b45as-960.jpg';
 const AVATAR_DEFAULT = 'https://yt3.ggpht.com/a/AGF-l79zTH00bozogFe1oAjQyY7-lnxI-uZMKgonrw=s900-mo-c-c0xffffffff-rj-k-no';
 const MAIN_URL = 'https://social-network.samuraijs.com/api/1.0/';
+const routes = {
+
+};
 
 export {
   BACKGROUND_IMG,
