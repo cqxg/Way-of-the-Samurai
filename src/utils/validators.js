098ebe35b7cb2c0f226f0fea48e31dd7bab3816c
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-14 22:25:48 +0300

    feat: add maxLength validator

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -3,5 +3,9 @@ const required = value => {
     return 'Field is required';
 };
 
+const maxLength30 = value => {
+    if (value.length > 30) return 'Max length is 30 symbols';
+    return undefined;
+};
 
 export { required };
