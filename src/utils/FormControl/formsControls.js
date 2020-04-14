bf216d96b718526d815c3b750f201f481ebd120f
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-14 22:59:12 +0300

    feat: add custom input in formsControls

--- src/utils/FormControl/formsControls.js
+++ src/utils/FormControl/formsControls.js
@@ -17,4 +17,19 @@ const Textarea = ({ input, meta, ...props }) => {
     )
 };
 
-export { Textarea };
+const Input = ({ input, meta, ...props }) => {
+    const hasError = meta.touched && meta.error;
+
+    return (
+        <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
+            <div>
+                <textarea {...input} {...props} />
+            </div>
+            <div>
+                {hasError && <span>{meta.error}</span>}
+            </div>
+        </div >
+    )
+};
+
+export { Textarea, Input };
