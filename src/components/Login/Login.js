7a97604b5826d25d9a195896173f25ba8e755a80
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-25 13:12:15 +0300

    feat: add redirect after login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import LoginReduxForm from './LoginForm';
 import { login } from '../../redux/actions/actionCreators';
@@ -12,6 +13,10 @@ const Login = (props) => {
     props.login(email, password, rememberMe);
   };
 
+  if (props.isAuth) {
+    return <Redirect to="/profile" />;
+  };
+
   return (
     <div>
       <h1>{I18N.EN.LOGIN}</h1>
@@ -20,4 +25,8 @@ const Login = (props) => {
   );
 };
 
-export default connect(null, { login })(Login);
+const mapStateToProps = (state) => ({
+  isAuth: state.auth.isAuth,
+});
+
+export default connect(mapStateToProps, { login })(Login);
