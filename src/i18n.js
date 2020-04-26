eb03fa3fbcb6a7346ee7d53213f3eafb1494caec
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-26 20:46:01 +0300

    feat: add i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,18 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import Backend from 'i18next-xhr-backend';
+
+i18n
+    .use(Backend)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'en',
+        defaultLocale: 'en',
+
+        interpolation: {
+            escapeValue: false,
+        }
+    });
+
+export default i18n;
