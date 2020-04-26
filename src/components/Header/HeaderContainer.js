cfb0b168a7671f9b05f61354f50aba296d70c105
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-26 20:31:14 +0300

    feat: logout implemented

--- src/components/Header/HeaderContainer.js
+++ src/components/Header/HeaderContainer.js
@@ -2,37 +2,37 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { getAuthUserData } from '../../redux/actions/actionCreators';
+import { getAuthUserData, logout } from '../../redux/actions/actionCreators';
 
 import Header from './Header';
 
 class HeaderContainer extends Component {
-  componentDidMount() {
-    const { props } = this;
-    props.getAuthUserData();
-  }
+    componentDidMount() {
+        const { props } = this;
+        props.getAuthUserData();
+    }
 
-  render() {
-    const { props } = this;
-    return <Header {...props} />;
-  }
-}
+    render() {
+        const { props } = this;
+        return <Header {...props} />;
+    }
+};
 
 const mapStateToProps = (state) => ({
-  isAuth: state.auth.isAuth,
-  login: state.auth.login,
+    isAuth: state.auth.isAuth,
+    login: state.auth.login,
 });
 
 
 HeaderContainer.defaultProps = {
-  setAuthUserData: PropTypes.func,
-  getAuthUserData: PropTypes.func,
+    setAuthUserData: PropTypes.func,
+    getAuthUserData: PropTypes.func,
 };
 
 HeaderContainer.propTypes = {
-  setAuthUserData: PropTypes.func,
-  getAuthUserData: PropTypes.func,
+    setAuthUserData: PropTypes.func,
+    getAuthUserData: PropTypes.func,
 };
 
 
-export default connect(mapStateToProps, { getAuthUserData })(HeaderContainer);
+export default connect(mapStateToProps, { getAuthUserData, logout })(HeaderContainer);
