b7d2c37b588aca11b231e0f3c1eeb50c10105983
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-28 00:14:30 +0300

    feat: add thunks.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-
 import Backend from 'i18next-xhr-backend';
 
 i18n
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import './i18n';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import './i18n';
 import store from './redux/store';
 import App from './App';
 
