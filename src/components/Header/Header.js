ab73c6680b959e80e58331ae537cf82ec0d1de54
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-28 00:33:00 +0300

    fix: indent

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,10 +25,7 @@ const Header = (props) => {
         {isAuth
           ? (
             <div>
-              <span>
-                {login}
--
-              </span>
+              <span>{login}-</span>
               <button onClick={props.logout}>{t('LOGOUT')}</button>
             </div>
           )
