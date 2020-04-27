6bdd3c1bb9b252373e4d13dd54c113505b0480f0
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-27 18:02:44 +0300

    feat: add useTranslation in LoginForm

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { useTranslation } from 'react-i18next';
 
 import { required } from '../../utils/validators';
 import { Input } from '../../utils/FormControl/formsControls';
-import { I18N } from '../../utils/constants';
 
 const LoginForm = (props) => {
-  const { handleSubmit } = props;
+    const { t } = useTranslation();
+    const { handleSubmit } = props;
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <Field placeholder="email" name="email" component={Input} validate={[required]} />
-      </div>
-      <div>
-        <Field placeholder="password" name="password" component={Input} validate={[required]} />
-      </div>
-      <div>
-        <Field name="rememberMe" type="checkbox" component="input" />
-        <span>{I18N.EN.REMEMBER_ME}</span>
-      </div>
-      <div>
-        <button>{I18N.EN.LOGIN}</button>
-      </div>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <Field placeholder="email" name="email" component={Input} validate={[required]} />
+            </div>
+            <div>
+                <Field placeholder="password" name="password" component={Input} validate={[required]} />
+            </div>
+            <div>
+                <Field name="rememberMe" type="checkbox" component="input" />
+                <span>{t('REMEMBER_ME')}</span>
+            </div>
+            <div>
+                <button>{t('LOGIN')}</button>
+            </div>
+        </form>
+    );
 };
 
 const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm);
