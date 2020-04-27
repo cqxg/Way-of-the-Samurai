6b5bffa2f89d89743cf5587cb36f99f790c176a1
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-27 18:01:01 +0300

    feat: add useTranslation in Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 import LoginReduxForm from './LoginForm';
 import { login } from '../../redux/actions/actionCreators';
 
-import { I18N } from '../../utils/constants';
-
 const Login = (props) => {
-  const onSubmit = (formData) => {
-    const { email, password, rememberMe } = formData;
-    props.login(email, password, rememberMe);
-  };
+    const { t } = useTranslation();
 
-  if (props.isAuth) {
-    return <Redirect to="/profile" />;
-  }
+    const onSubmit = (formData) => {
+        const { email, password, rememberMe } = formData;
+        props.login(email, password, rememberMe);
+    };
 
-  return (
-    <div>
-      <h1>{I18N.EN.LOGIN}</h1>
-      <LoginReduxForm onSubmit={onSubmit} />
-    </div>
-  );
+    if (props.isAuth) {
+        return <Redirect to="/profile" />;
+    };
+
+    return (
+        <div>
+            <h1>{t('LOGIN')}</h1>
+            <LoginReduxForm onSubmit={onSubmit} />
+        </div>
+    );
 };
 
 const mapStateToProps = (state) => ({
-  isAuth: state.auth.isAuth,
+    isAuth: state.auth.isAuth,
 });
 
 export default connect(mapStateToProps, { login })(Login);
