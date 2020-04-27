d9135464b9a0d55958292cd8a2adb9b3fdff825c
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-27 17:34:00 +0300

    feat: add useTranslation in message form

--- src/components/Dialogs/Message/AddMessageForm.js
+++ src/components/Dialogs/Message/AddMessageForm.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { useTranslation } from 'react-i18next';
 
 import { required, maxLengthCreator } from '../../../utils/validators';
 import { Textarea } from '../../../utils/FormControl/formsControls';
-import { I18N } from '../../../utils/constants';
 
 const maxLenght10 = maxLengthCreator(10);
 
 const AddMessageForm = (props) => {
-  const { handleSubmit } = props;
+    const { t } = useTranslation();
+    const { handleSubmit } = props;
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <Field component={Textarea} name="newMessageBody" validate={[required, maxLenght10]} />
-      </div>
-      <div>
-        <button>{I18N.EN.SEND}</button>
-      </div>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <Field component={Textarea} name="newMessageBody" validate={[required, maxLenght10]} />
+            </div>
+            <div>
+                <button>{t('SEND')}</button>
+            </div>
+        </form>
+    );
 };
 
 const AddMessageFormRedux = reduxForm({ form: 'dialogAddMessageForm' })(AddMessageForm);
