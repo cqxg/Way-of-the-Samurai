63d4731e92fb3d08781895794e0ff4ab71f8fcf7
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-27 18:08:30 +0300

    feat: add useTranslation in Navnar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
-import { I18N } from '../../utils/constants';
 import { ROUTES } from '../../utils/url-utils';
 
 import style from './Navbar.module.css';
 
-const Navbar = () => (
-  <nav className={style.nav}>
-    <div className={style.item}>
-      <NavLink to={ROUTES.PROFILE} activeClassName={style.activeLink}>{I18N.EN.PROFILE}</NavLink>
-    </div>
-    <div className={`${style.item} ${style.active}`}>
-      <NavLink to={ROUTES.DIALOGS} activeClassName={style.activeLink}>{I18N.EN.MESSAGES}</NavLink>
-    </div>
-    <div className={`${style.item} ${style.active}`}>
-      <NavLink to={ROUTES.USERS} activeClassName={style.activeLink}>{I18N.EN.USERS}</NavLink>
-    </div>
-    <div className={style.item}>
-      <NavLink to={ROUTES.NEWS} activeClassName={style.activeLink}>{I18N.EN.NEWS}</NavLink>
-    </div>
-    <div className={style.item}>
-      <NavLink to={ROUTES.MUSIC} activeClassName={style.activeLink}>{I18N.EN.MUSIC}</NavLink>
-    </div>
-    <div className={style.item}>
-      <NavLink to={ROUTES.SETTINGS} activeClassName={style.activeLink}>{I18N.EN.SETTINGS}</NavLink>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+    const { t } = useTranslation();
+
+    return (
+        <nav className={style.nav}>
+            <div className={style.item}>
+                <NavLink to={ROUTES.PROFILE} activeClassName={style.activeLink}>{t('PROFILE')}</NavLink>
+            </div>
+            <div className={`${style.item} ${style.active}`}>
+                <NavLink to={ROUTES.DIALOGS} activeClassName={style.activeLink}>{t('MESSAGES')}</NavLink>
+            </div>
+            <div className={`${style.item} ${style.active}`}>
+                <NavLink to={ROUTES.USERS} activeClassName={style.activeLink}>{t('USERS')}</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to={ROUTES.NEWS} activeClassName={style.activeLink}>{t('NEWS')}</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to={ROUTES.MUSIC} activeClassName={style.activeLink}>{t('MUSIC')}</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to={ROUTES.SETTINGS} activeClassName={style.activeLink}>{t('SETTINGS')}</NavLink>
+            </div>
+        </nav>
+    )
+};
 
 export default Navbar;
