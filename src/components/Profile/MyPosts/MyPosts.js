5418ba9a44030f69d919820823ffc7308d3159a5
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-27 18:12:20 +0300

    feat: add useTranslation in posts

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -1,55 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
 
 import Post from './Post/Post';
 import AddNewPostForm from './AddNewPostForm';
 
-import { I18N } from '../../../utils/constants';
-
 import style from './MyPosts.module.css';
 
 const MyPosts = (props) => {
-  const {
-    posts,
-    addPost,
-  } = props;
+    const { t } = useTranslation();
 
-  const postsElements = posts.map((post) => {
-    const { message, likesCount } = post;
+    const {
+        posts,
+        addPost,
+    } = props;
+
+    const postsElements = posts.map((post) => {
+        const { message, likesCount } = post;
+        return (
+            <Post
+                key={message}
+                message={message}
+                likesCount={likesCount}
+            />
+        );
+    });
+
+    const onAddPost = (values) => {
+        addPost(values.newPostText);
+    };
+
     return (
-      <Post
-        key={message}
-        message={message}
-        likesCount={likesCount}
-      />
+        <div className={style.postsBlock}>
+            <h3>{t('MY_POSTS')}</h3>
+            <AddNewPostForm onSubmit={onAddPost} />
+            <div className={style.posts}>
+                {postsElements}
+            </div>
+        </div>
     );
-  });
-
-  const onAddPost = (values) => {
-    addPost(values.newPostText);
-  };
-
-  return (
-    <div className={style.postsBlock}>
-      <h3>{I18N.EN.MY_POSTS}</h3>
-      <AddNewPostForm onSubmit={onAddPost} />
-      <div className={style.posts}>
-        {postsElements}
-      </div>
-    </div>
-  );
 };
 
 MyPosts.defaultProps = {
-  posts: PropTypes.array,
-  addPost: PropTypes.func,
-  updateNewPostText: PropTypes.func,
-  newPostText: PropTypes.string,
+    posts: PropTypes.array,
+    addPost: PropTypes.func,
+    updateNewPostText: PropTypes.func,
+    newPostText: PropTypes.string,
 };
 
 MyPosts.propTypes = {
-  posts: PropTypes.instanceOf(Array),
-  addPost: PropTypes.func,
+    posts: PropTypes.instanceOf(Array),
+    addPost: PropTypes.func,
 };
 
 export default MyPosts;
