db3ca6b346d5b3e36f85bb48b50c9aed52b7f72f
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-28 18:57:33 +0300

    feat: add new I18N obj in constants

--- src/i18n.js
+++ src/i18n.js
@@ -8,7 +8,6 @@ i18n
   .init({
     fallbackLng: 'en',
     defaultLocale: 'en',
-    debug: true,
 
     interpolation: {
       escapeValue: false,
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,6 +5,10 @@ const API_KEY = '97590639-9a46-4b9d-a8e4-899e90470cbc';
 const ALT_PROFILE_AVATAR = 'Avatar';
 const ALT_USER_AVATAR = 'User Avatar';
 const WELL = 0;
+const I18N = {
+    ru: 'ru',
+    en: 'en'
+}
 
 export {
   DEFAULT_LIKES_VALUE, NEXT_ID, DEFAULT_USER_ID, API_KEY, ALT_PROFILE_AVATAR, ALT_USER_AVATAR, WELL,
