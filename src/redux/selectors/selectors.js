ac0d775b41e60c4bfde6bde496695f9ade2dd9f9
Ihar Ivaniuk <cqxg@yandex.ru>
2020-05-01 22:56:00 +0300

    feat: add example of the use of reselect

--- src/redux/selectors/selectors.js
+++ src/redux/selectors/selectors.js
@@ -1,4 +1,14 @@
-const takeUser = (state) => state.usersPage.users;
+import { createSelector } from 'reselect';
+
+const takeUserSelector = (state) => {
+    return state.usersPage.users
+};
+
+const takeUser = createSelector(takeUserSelector,
+    (users) => {
+        return users.filter(u => true);
+    }
+);
 
 const getPageSize = (state) => state.usersPage.pageSize;
 
@@ -11,10 +21,10 @@ const getIsFetching = (state) => state.usersPage.isFetching;
 const getFollowingInProgress = (state) => state.usersPage.followingInProgress;
 
 export {
-  takeUser,
-  getPageSize,
-  getIsFetching,
-  getCurrentPage,
-  getTotalUsersCount,
-  getFollowingInProgress,
+    takeUser,
+    getPageSize,
+    getIsFetching,
+    getCurrentPage,
+    getTotalUsersCount,
+    getFollowingInProgress,
 };
