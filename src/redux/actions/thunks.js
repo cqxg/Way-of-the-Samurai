41c62730b28c073f3409b8ffcc0d0e147534a5e1
Ihar Ivaniuk <cqxg@yandex.ru>
2020-05-17 22:19:34 +0300

    feat: a little bit practice with async / await

--- src/redux/actions/thunks.js
+++ src/redux/actions/thunks.js
@@ -5,112 +5,108 @@ import { WELL } from '../../utils/constants';
 import { usersAPI, authAPI } from '../../api/api';
 
 import {
-    setUsers,
-    setStatus,
-    followSuccess,
-    setUserProfile,
-    setCurrentPage,
-    setInitialized,
-    unfollowSuccess,
-    setAuthUserData,
-    toggleIsFetching,
-    setTotalUsersCount,
-    toggleFollowingProgress,
+  setUsers,
+  setStatus,
+  followSuccess,
+  setUserProfile,
+  setCurrentPage,
+  setInitialized,
+  unfollowSuccess,
+  setAuthUserData,
+  toggleIsFetching,
+  setTotalUsersCount,
+  toggleFollowingProgress,
 } from './actionCreators';
 
 const initializeApp = () => (dispatch) => {
-    const promise = dispatch(getAuthUserData());
-    Promise.all([promise]).then(() => {
-        dispatch(setInitialized());
-    });
+  const promise = dispatch(getAuthUserData());
+  Promise.all([promise]).then(() => {
+    dispatch(setInitialized());
+  });
 };
 
 const getAuthUserData = () => async (dispatch) => {
-    const response = await authAPI.me();
-    if (response.data.resultCode === WELL) {
-        const { id, email, login } = response.data.data;
-        dispatch(setAuthUserData(id, email, login, true));
-    }
+  const response = await authAPI.me();
+  if (response.data.resultCode === WELL) {
+    const { id, email, login } = response.data.data;
+    dispatch(setAuthUserData(id, email, login, true));
+  }
 };
 
 const login = (email, password, rememberMe) => async (dispatch) => {
-    const response = await authAPI.login(email, password, rememberMe);
-    if (response.data.resultCode === WELL) {
-        dispatch(getAuthUserData());
-    } else {
-        const mess = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error';
-        dispatch(stopSubmit('login', { _error: mess }));
-    };
+  const response = await authAPI.login(email, password, rememberMe);
+  if (response.data.resultCode === WELL) {
+    dispatch(getAuthUserData());
+  } else {
+    const mess = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error';
+    dispatch(stopSubmit('login', { _error: mess }));
+  }
 };
 
 const logout = () => async (dispatch) => {
-    const response = await authAPI.logout();
-    if (response.data.resultCode === WELL) {
-        dispatch(setAuthUserData(null, null, null, false));
-    };
+  const response = await authAPI.logout();
+  if (response.data.resultCode === WELL) {
+    dispatch(setAuthUserData(null, null, null, false));
+  }
 };
 
 const getUsers = (currentPage, pageSize) => (dispatch) => {
-    dispatch(toggleIsFetching(true));
-    dispatch(setCurrentPage(currentPage));
-    usersAPI.getUsers(currentPage, pageSize).then((data) => {
-        dispatch(toggleIsFetching(false));
-        dispatch(setUsers(data.items));
-        dispatch(setTotalUsersCount(data.totalCount));
-    });
+  dispatch(toggleIsFetching(true));
+  dispatch(setCurrentPage(currentPage));
+  usersAPI.getUsers(currentPage, pageSize).then((data) => {
+    dispatch(toggleIsFetching(false));
+    dispatch(setUsers(data.items));
+    dispatch(setTotalUsersCount(data.totalCount));
+  });
 };
 
-const getUserProfile = (userId) => (dispatch) => {
-    usersAPI.getProfile(userId).then((response) => {
-        dispatch(setUserProfile(response.data));
-    });
+const getUserProfile = (userId) => async (dispatch) => {
+  const response = await usersAPI.getProfile(userId);
+  dispatch(setUserProfile(response.data));
 };
 
-const getStatus = (userId) => (dispatch) => {
-    usersAPI.getStatus(userId).then((response) => {
-        dispatch(setStatus(response.data));
-    });
+const getStatus = (userId) => async (dispatch) => {
+  const response = await usersAPI.getStatus(userId);
+  dispatch(setStatus(response.data));
 };
 
-const updateStatus = (status) => (dispatch) => {
-    usersAPI.updateStatus(status).then((response) => {
-        if (response.data.resultCode === 0) {
-            dispatch(setStatus(response.data));
-        }
-    });
+const updateStatus = (status) => async (dispatch) => {
+  const response = usersAPI.updateStatus(status);
+  if (response.data.resultCode === 0) {
+    dispatch(setStatus(response.data));
+  }
 };
 
 const follow = (userId) => (dispatch) => {
-    dispatch(toggleFollowingProgress(true, userId));
-
-    usersAPI.follow(userId).then((response) => {
-        if (response.data.resultCode === 0) {
-            dispatch(followSuccess(userId));
-        }
-        dispatch(toggleFollowingProgress(false, userId));
-    });
+  dispatch(toggleFollowingProgress(true, userId));
+  usersAPI.follow(userId).then((response) => {
+    if (response.data.resultCode === 0) {
+      dispatch(followSuccess(userId));
+    }
+    dispatch(toggleFollowingProgress(false, userId));
+  });
 };
 
 const unfollow = (userId) => (dispatch) => {
-    dispatch(toggleFollowingProgress(true, userId));
+  dispatch(toggleFollowingProgress(true, userId));
 
-    usersAPI.unfollow(userId).then((response) => {
-        if (response.data.resultCode === 0) {
-            dispatch(unfollowSuccess(userId));
-        }
-        dispatch(toggleFollowingProgress(false, userId));
-    });
+  usersAPI.unfollow(userId).then((response) => {
+    if (response.data.resultCode === 0) {
+      dispatch(unfollowSuccess(userId));
+    }
+    dispatch(toggleFollowingProgress(false, userId));
+  });
 };
 
 export {
-    login,
-    logout,
-    follow,
-    unfollow,
-    getUsers,
-    getStatus,
-    updateStatus,
-    initializeApp,
-    getUserProfile,
-    getAuthUserData,
+  login,
+  logout,
+  follow,
+  unfollow,
+  getUsers,
+  getStatus,
+  updateStatus,
+  initializeApp,
+  getUserProfile,
+  getAuthUserData,
 };
