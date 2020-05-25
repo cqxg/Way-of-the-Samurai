1df69fa5a5e6bd975ba23745e45c52323de89912
unknown <cqxg@yandex.ru>
2020-05-25 18:56:14 +0300

    feat: pages count test improved

--- src/utils/Paginator/paginator.test.js
+++ src/utils/Paginator/paginator.test.js
@@ -10,4 +10,10 @@ describe("Paginator component tests", () => {
         let spans = root.findAllByType("span");
         expect(spans.length).toBe(10);
     });
+    test("if pages count is more then 10 button NEXT should be present", () => {
+        const component = create(<Paginator totalItemsCount={11} pageSize={1} portionSize={10} />);
+        const root = component.root;
+        let button = root.findAllByType("button");
+        expect(button.length).toBe(1);
+    });
 });
