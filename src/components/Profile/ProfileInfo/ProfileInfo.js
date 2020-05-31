46fdecb0e850acc5cc6ad4969454915909a296ed
unknown <cqxg@yandex.ru>
2020-05-31 12:54:52 +0300

    feat: add profile filling blank

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -28,6 +28,28 @@ const ProfileInfo = (props) => {
       <div className={style.descriptionBlock}>
         <img className={style.mainPhotos} alt="" src={profile.photos.large || AVATAR_DEFAULT} />
         {isOwner && <input type="file" onChange={onMainPhotoSelected} />}
+        <div>
+          <b>Full name</b>: {profile.fullName}
+        </div>
+        <div>
+          <b>Looking for a job</b>: {profile.lookingForAJob ? "yes" : "no"}
+        </div>
+        {profile.lookingForAJob &&
+          <div>
+            <b>My professional skills</b>: {profile.lookingForAJobDescription}
+          </div>
+        }
+
+        <div>
+          <b>About me</b>: {profile.aboutMe}
+        </div>
+        <div>
+          <b>Contacts</b>: {
+            Object.keys(profile.contacts).map((key) => {
+              return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]} />
+            })}
+        </div>
+
         <ProfileStatus status={status} updateStatus={updateStatus} />
       </div>
     </div>
@@ -41,5 +63,9 @@ ProfileInfo.defaultProps = {
 ProfileInfo.propTypes = {
   profile: PropTypes.instanceOf(Object),
 };
+
+const Contact = ({ contactTitle, contactValue }) => {
+  return <div className={style.contact}><b>{contactTitle}</b>: {contactValue}</div>
+}
 
 export default ProfileInfo;
