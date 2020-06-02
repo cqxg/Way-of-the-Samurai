b5d4d2c03e47dbee9d33c67a221a697a8389ea79
unknown <cqxg@yandex.ru>
2020-06-02 21:04:24 +0300

    fix: breaking JSX in ProfileDataForm into separate functions

--- src/components/Profile/ProfileInfo/ProfileData/ProfileDataForm.js
+++ src/components/Profile/ProfileInfo/ProfileData/ProfileDataForm.js
@@ -1,40 +1,63 @@
 import React from 'react';
 import { reduxForm } from 'redux-form';
 
-import { createField, Input, Textarea } from '../../../../utils/FormControl/formsControls';
+import {
+  createField,
+  Input,
+  Textarea,
+} from '../../../../utils/FormControl/formsControls';
 
 import Contact from './Contact';
 
 const ProfileDataForm = (props) => {
   const { profile, handleSubmit } = props;
+
+  const contacts = Object.keys(profile.contacts);
+  const contactsMap = contacts.map((key) => <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]} />);
+
+  const fullnameRender = () => (
+    <div>
+      <b>Full name: </b>
+      {createField('Full name', 'fullName', [], Input)}
+    </div>
+  );
+
+  const forJobRender = () => (
+    <div>
+      <b>Looking for a job: </b>
+      {createField('', 'lookingForAJob', [], Input, { type: 'checkbox' })}
+    </div>
+  );
+
+  const skillsRender = () => (
+    <div>
+      <b>My professional skills: </b>
+      {createField('My professional skills', 'lookingForAJobDescription', [], Textarea)}
+    </div>
+  );
+
+  const aboutMeRender = () => (
+    <div>
+      <b>About me: </b>
+      {createField('About me', 'aboutMe', [], Textarea)}
+    </div>
+  );
+
+  const contactsRender = () => (
+    <div>
+      <b>Contacts: </b>
+      {contactsMap}
+    </div>
+  );
+
   return (
     <form onSubmit={handleSubmit}>
       <div><button>save</button></div>
-      <div>
-        <b>Full name</b>
-:
-        {createField('Full name', 'fullName', [], Input)}
-      </div>
-      <div>
-        <b>Looking for a job</b>
-:
-        {createField('', 'lookingForAJob', [], Input, { type: 'checkbox' })}
-      </div>
-
-      <div>
-        <b>My professional skills</b>
-:
-        {createField('My professional skills', 'lookingForAJobDescription', [], Textarea)}
-      </div>
-      <div>
-        <b>About me</b>
-:
-        {createField('About me', 'aboutMe', [], Textarea)}
-      </div>
-      <div>
-        <b>Contacts</b>
-        {Object.keys(profile.contacts).map((key) => <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]} />)}
-      </div>
+      {fullnameRender()}
+      {forJobRender()}
+      {skillsRender()}
+      {aboutMeRender()}
+      {contactsRender()}
     </form>
   );
 };
